f8677bcd286ee937f6c34056f76d29ac0583f675
PortfolioAI <135471798+PortfolioAI@users.noreply.github.com>
2023-06-08 23:08:25 -0400

    Update life.py

--- life.py
+++ life.py
@@ -1,63 +1,53 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
+from scipy.ndimage import convolve
+import matplotlib.colors as mcolors
 
 # Set the size of the grid
 N = 200
 
+# Initialize the random state for reproducibility
+np.random.seed(0)
+
 # Set up the initial state of the grid
-grid = np.random.choice([0, 1], size=(N, N), p=[0.5, 0.5])
+grid = np.random.choice([0, 1], size=(N, N), p=[0.5, 0.5]).astype(np.uint8)
 
-# Define the rules for the Game of Life
-def get_next_generation(grid):
-    next_generation = np.zeros_like(grid)
-    for i in range(N):
-        for j in range(N):
-            num_neighbors = count_neighbors(grid, i, j)
-            next_generation[i, j] = apply_rules(grid, i, j, num_neighbors)
-    return next_generation
+# Define the neighborhood kernel: 3x3 matrix with 1s surrounding a 0
+kernel = np.array([[1, 1, 1],
+                   [1, 0, 1],
+                   [1, 1, 1]])
 
-def count_neighbors(grid, i, j):
-    """Count the number of live neighbors around a cell."""
-    neighbors = (
-        grid[(i-1)%N, (j-1)%N] + grid[(i-1)%N, j] + grid[(i-1)%N, (j+1)%N] +
-        grid[i, (j-1)%N] + grid[i, (j+1)%N] +
-        grid[(i+1)%N, (j-1)%N] + grid[(i+1)%N, j] + grid[(i+1)%N, (j+1)%N]
-    )
-    return neighbors
+def compute_next_generation(grid):
+    """Return the next generation of the grid in the Game of Life."""
+    # Count the number of live neighbors for each cell
+    num_neighbors = convolve(grid, kernel, mode='wrap')
 
-def apply_rules(grid, i, j, num_neighbors):
-    """Apply the rules of the Game of Life."""
-    if grid[i, j] == 1 and (num_neighbors < 2 or num_neighbors > 3):
-        return 0
-    elif grid[i, j] == 0 and num_neighbors == 3:
-        return 1
-    else:
-        return grid[i, j]
+    # Apply the rules of the Game of Life using logical operations
+    return ((grid == 1) & (num_neighbors >= 2) & (num_neighbors <= 3)) | ((grid == 0) & (num_neighbors == 3))
 
 # Set up the plot
-fig, ax = plt.subplots()
-im = ax.imshow(grid, cmap='binary')
+fig, ax = plt.subplots(figsize=(8, 8))
 
-# Define the animation function
-def animate(frame):
-    global grid
-    grid = get_next_generation(grid)
-    im.set_data(grid)
-    return [im]
+# Use a colormap to make the plot more colorful
+cmap = mcolors.LinearSegmentedColormap.from_list("MyCmap", ["black", "green"])
 
-# Start the animation
-ani = animation.FuncAnimation(fig, animate, frames=1000, interval=50, blit=True)
-
-# Set the axis limits of the plot
-ax.set_xlim([0, N])
-ax.set_ylim([0, N])
-
-# Hide the axis ticks and labels
+image = ax.imshow(grid, cmap=cmap, interpolation='nearest')
 ax.set_xticks([])
 ax.set_yticks([])
-ax.set_xticklabels([])
-ax.set_yticklabels([])
 
-# Make sure the animation window stays open until closed by the user
+# Add a colorbar
+cbar = plt.colorbar(image, ax=ax, ticks=[0, 1])
+cbar.ax.set_yticklabels(['Dead', 'Alive'])
+
+def update_image(frame, grid, image):
+    """Update the image for a new frame."""
+    grid[:] = compute_next_generation(grid)
+    image.set_array(grid)
+    return [image]
+
+# Start the animation
+ani = animation.FuncAnimation(fig, update_image, frames=1000, interval=50, blit=True, fargs=(grid, image))
+
+# Display the animation
 plt.show()

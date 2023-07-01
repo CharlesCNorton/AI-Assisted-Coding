f1e3980c9c582afa07990ccad898a01664b74cdf
PortfolioAI <135471798+PortfolioAI@users.noreply.github.com>
2023-06-30 22:19:51 -0700

    Overhaul of Brick Breaker with new gameplay mechanics and sound effects
    Updated the game to include a range of new features including dynamic ball speed, enhanced scoring system, colored bricks, and immersive sound effects. The ball's trajectory now alters based on paddle impact, adding a layer of depth to the game. The paddle's movement influences the ball's speed, and new 'bonk' sound effects have been introduced for better gameplay experience.

--- BrickBreaker/brickbreaker.py
+++ BrickBreaker/brickbreaker.py
@@ -1,8 +1,11 @@
 import sys
 import pygame
+import numpy as np
+import scipy.io.wavfile as wavfile
 
 # Initialize Pygame
 pygame.init()
+pygame.mixer.init()
 
 # Define constants
 WIDTH, HEIGHT = 800, 600
@@ -11,14 +14,56 @@ BALL_RADIUS = 8
 BRICK_WIDTH, BRICK_HEIGHT = 75, 20
 ROWS, COLUMNS = 5, 10
 WHITE, BLACK = (255, 255, 255), (0, 0, 0)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
 PADDLE_SPEED = 5
 BALL_SPEED = [2, -2]
+
+def generate_bonk_sound():
+    # Sample rate and time
+    sample_rate = 44100
+    duration = 0.05
+
+    # Frequency for 'bonk' sound
+    freq = 440.0
+
+    # Create an array for the sound
+    t = np.linspace(0, duration, int(sample_rate * duration), False)
+
+    # Generate a 'bonk' sound
+    bonk_sound = np.sin(freq * t * 2 * np.pi)
+
+    # Fade in/out
+    fade_in = np.linspace(0., 1., int(sample_rate * duration // 10))
+    fade_out = np.linspace(1., 0., int(sample_rate * duration // 5))
+
+    bonk_sound[:fade_in.size] = np.multiply(bonk_sound[:fade_in.size], fade_in)
+    bonk_sound[-fade_out.size:] = np.multiply(bonk_sound[-fade_out.size:], fade_out)
+
+    # Convert to 16-bit PCM sound
+    bonk_sound = (bonk_sound * 32767 / np.max(np.abs(bonk_sound))).astype(np.int16)
+
+    # Convert to stereo sound by duplicating the mono sound
+    bonk_sound_stereo = np.vstack([bonk_sound, bonk_sound]).T
+
+    # Make the numpy array C-contiguous
+    bonk_sound_stereo = np.ascontiguousarray(bonk_sound_stereo)
+
+    # Write to BytesIO object
+    bonk_sound_io = pygame.sndarray.make_sound(bonk_sound_stereo)
+
+    return bonk_sound_io
+
+# Create bonk sound
+bonk_sound = generate_bonk_sound()
 
 class Paddle(pygame.Rect):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+        self.last_x = self.x  # Track the last position
 
     def move_paddle(self, speed):
+        self.last_x = self.x  # Save the current position before moving
         keys = pygame.key.get_pressed()
         if keys[pygame.K_LEFT]:
             self.move_ip(-speed, 0)
@@ -30,6 +75,10 @@ class Paddle(pygame.Rect):
         if self.left < 0:
             self.left = 0
 
+    @property
+    def velocity(self):
+        return self.x - self.last_x  # Calculate the velocity
+
 class Ball(pygame.Rect):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
@@ -40,12 +89,15 @@ class Ball(pygame.Rect):
         # Collisions with screen edges
         if self.left < 0 or self.right > WIDTH:
             speed[0] = -speed[0]
+            bonk_sound.play()
         if self.top < 0:
             speed[1] = -speed[1]
+            bonk_sound.play()
 
 class Brick(pygame.Rect):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+        self.color = WHITE  # default color
 
 class Game:
     def __init__(self):
@@ -54,8 +106,18 @@ class Game:
         self.font = pygame.font.Font(None, 36)
         self.score = 0
         self.paddle = Paddle(WIDTH // 2 - PADDLE_WIDTH // 2, HEIGHT - 50, PADDLE_WIDTH, PADDLE_HEIGHT)
-        self.ball = Ball(WIDTH // 2 - BALL_RADIUS, HEIGHT // 2 - BALL_RADIUS, BALL_RADIUS * 2, BALL_RADIUS * 2)
-        self.bricks = [Brick(x * (BRICK_WIDTH + 5), y * (BRICK_HEIGHT + 5) + 50, BRICK_WIDTH, BRICK_HEIGHT) for x in range(COLUMNS) for y in range(ROWS)]
+        self.ball = Ball(WIDTH // 2 - BALL_RADIUS // 2, HEIGHT // 2 - BALL_RADIUS // 2, BALL_RADIUS * 2, BALL_RADIUS * 2)
+
+        # Initialize bricks
+        self.bricks = []
+        for x in range(COLUMNS):
+            for y in range(ROWS):
+                brick = Brick(x * (BRICK_WIDTH + 5), y * (BRICK_HEIGHT + 5) + 50, BRICK_WIDTH, BRICK_HEIGHT)
+                if y < ROWS // 2:  # set color based on position or any other logic
+                    brick.color = RED
+                else:
+                    brick.color = GREEN
+                self.bricks.append(brick)
 
     def run(self):
         while True:
@@ -76,13 +138,19 @@ class Game:
         self.ball.move_ball(BALL_SPEED)
 
         if self.ball.colliderect(self.paddle):
-            BALL_SPEED[1] = -BALL_SPEED[1]
+            # Calculate where the ball hit the paddle and change direction accordingly
+            hit_location = (self.ball.centerx - self.paddle.left) / self.paddle.width
+            BALL_SPEED[0] = (hit_location - 0.5) * 4  # Adjust this to change bounce "sharpness"
+            BALL_SPEED[0] += self.paddle.velocity / 10  # Add paddle velocity
+            BALL_SPEED[1] = -abs(BALL_SPEED[1])  # Always bounce upwards
+            bonk_sound.play()
 
         for brick in self.bricks:
             if self.ball.colliderect(brick):
                 BALL_SPEED[1] = -BALL_SPEED[1]
                 self.bricks.remove(brick)
                 self.score += 1
+                bonk_sound.play()
                 break
 
         if self.ball.bottom > HEIGHT:
@@ -90,6 +158,7 @@ class Game:
 
         if not self.bricks:
             self.end_game("You Win!")
+
 
     def end_game(self, text):
         self.render_text(text, (WIDTH // 2 - 80, HEIGHT // 2 - 20))
@@ -100,14 +169,31 @@ class Game:
     def render_objects(self):
         self.screen.fill(BLACK)
         pygame.draw.rect(self.screen, WHITE, self.paddle)
-        pygame.draw.circle(self.screen, WHITE, self.ball.center, BALL_RADIUS)
+        pygame.draw.circle(self.screen, WHITE, (self.ball.x + BALL_RADIUS, self.ball.y + BALL_RADIUS), BALL_RADIUS)
         for brick in self.bricks:
-            pygame.draw.rect(self.screen, WHITE, brick)
+            pygame.draw.rect(self.screen, brick.color, brick)
+
         self.render_text(f"Score: {self.score}", (10, 10))
 
     def render_text(self, text, position):
         text_surface = self.font.render(text, True, WHITE)
         self.screen.blit(text_surface, position)
 
+    def show_menu(self):
+        while True:
+            self.screen.fill(BLACK)
+            self.render_text("Brick Breaker", (WIDTH // 2 - 80, HEIGHT // 3))
+            self.render_text("Press SPACE to start", (WIDTH // 2 - 120, HEIGHT // 2))
+            pygame.display.flip()
+
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
+                if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
+                    return  # start the game
+
 if __name__ == "__main__":
-    Game().run()
+    game = Game()
+    game.show_menu()
+    game.run()
--- BrickBreaker/readme.txt.txt
+++ BrickBreaker/readme.txt.txt
@@ -1,25 +1,24 @@
-# Pygame Brick Breaker
+Advanced Pygame Brick Breaker
+Welcome to our freshly revamped Brick Breaker game, now packing an arsenal of new features! This enhanced edition, coded in Python using the Pygame library, offers an even more engaging gaming experience.
 
-This repository contains a simple but fun Brick Breaker game coded using Python's Pygame library.
+Updated Game Features
+Player-Controlled Paddle and Ball: Just like before, control a paddle to keep the ball in play, but this time with added dynamics. The ball's trajectory varies based on where it hits the paddle, giving you more control over your strategy.
 
-## Game Features
+Dynamic Ball Speed: To add a touch of unpredictability and challenge, the ball speed now adjusts according to the motion of the paddle, making the game play more lively and adding a layer of skill.
 
-1. The game has a player-controlled paddle and a ball. The goal is to destroy all the bricks by hitting them with the ball without letting the ball fall past the paddle.
+Enhanced Scoring System: Enjoy the thrill of your score increasing with each brick you shatter. The game keeps track of your score and displays it on the screen.
 
-2. The game contains a scoring system, where the player's score increases with each brick they destroy.
+Colorful Bricks: The bricks now come in different colors, adding a visual appeal to your gaming experience.
 
-3. The paddle stays within the screen boundaries to ensure smooth gameplay.
+Sound Effects: Immerse yourself in the gameplay with our new 'bonk' sound effects that play on collision.
 
-4. When the ball hits the bottom of the screen, the game ends, and a "Game Over" message is displayed.
+End Game Indicators: The game concludes with a "Game Over" message when the ball hits the bottom. If you prove victorious and destroy all the bricks, a "You Win!" message is displayed.
 
-5. If the player successfully destroys all the bricks, a "You Win" message is displayed, signifying the end of the game.
+How to Play
+Make sure Python and Pygame are installed on your computer. Then, run the brick_breaker.py file in your Python environment to start the fun.
 
-## How to Run the Game
+Controls
+Use the left and right arrow keys to move your paddle. The ball moves automatically.
 
-Ensure you have Python and Pygame installed on your computer. Then, run the `brick_breaker.py` file in your Python environment to start the game.
-
-## Controls
-
-Use the left and right arrow keys to move the paddle. The ball moves automatically.
-
-Enjoy the game!
+Start the Fun
+Our new Brick Breaker game is sure to provide hours of entertainment. Jump in and see how many bricks you can smash!

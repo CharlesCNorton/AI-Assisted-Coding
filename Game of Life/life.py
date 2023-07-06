fd0e6e60ef7629f799fa718908462a56bcf7844c
PortfolioAI <135471798+PortfolioAI@users.noreply.github.com>
2023-07-06 07:44:39 -0400

    Improved robustness/E.handling
    This commit adds better error handling and robustness to the Game of Life simulation script:
    
    - Added checks for grid size to ensure it's neither too small nor too large.
    - Verified that probabilities used for grid initialization sum to 1.
    - Implemented try-except blocks around critical operations like grid initialization, convolution, plot setup, and animation display to catch and raise relevant exceptions.

--- Game of Life/life.py
+++ Game of Life/life.py
@@ -4,14 +4,22 @@ import matplotlib.animation as animation
 from scipy.ndimage import convolve
 import matplotlib.colors as mcolors
 
-# Set the size of the grid
-N = 200
+# Define the size of the grid, ensure it's not too small or too large
+N = min(max(3, 200), 1000)
 
 # Initialize the random state for reproducibility
 np.random.seed(0)
 
+# Check that the probabilities sum to 1 to avoid unexpected behaviour
+probabilities = [0.5, 0.5]
+assert abs(sum(probabilities) - 1) < 1e-9, "Probabilities must sum to 1"
+
 # Set up the initial state of the grid
-grid = np.random.choice([0, 1], size=(N, N), p=[0.5, 0.5]).astype(np.uint8)
+try:
+    grid = np.random.choice([0, 1], size=(N, N), p=probabilities).astype(np.uint8)
+except ValueError as e:
+    print("Invalid input for initial state of the grid. Error: ", str(e))
+    raise
 
 # Define the neighborhood kernel: 3x3 matrix with 1s surrounding a 0
 kernel = np.array([[1, 1, 1],
@@ -21,24 +29,36 @@ kernel = np.array([[1, 1, 1],
 def compute_next_generation(grid):
     """Return the next generation of the grid in the Game of Life."""
     # Count the number of live neighbors for each cell
-    num_neighbors = convolve(grid, kernel, mode='wrap')
+    try:
+        num_neighbors = convolve(grid, kernel, mode='wrap')
+    except Exception as e:
+        print("Error during convolution. Error: ", str(e))
+        raise
 
     # Apply the rules of the Game of Life using logical operations
     return ((grid == 1) & (num_neighbors >= 2) & (num_neighbors <= 3)) | ((grid == 0) & (num_neighbors == 3))
 
 # Set up the plot
-fig, ax = plt.subplots(figsize=(8, 8))
+try:
+    fig, ax = plt.subplots(figsize=(8, 8))
+except Exception as e:
+    print("Error setting up the plot. Error: ", str(e))
+    raise
 
 # Use a colormap to make the plot more colorful
 cmap = mcolors.LinearSegmentedColormap.from_list("MyCmap", ["black", "green"])
 
-image = ax.imshow(grid, cmap=cmap, interpolation='nearest')
-ax.set_xticks([])
-ax.set_yticks([])
+try:
+    image = ax.imshow(grid, cmap=cmap, interpolation='nearest')
+    ax.set_xticks([])
+    ax.set_yticks([])
 
-# Add a colorbar
-cbar = plt.colorbar(image, ax=ax, ticks=[0, 1])
-cbar.ax.set_yticklabels(['Dead', 'Alive'])
+    # Add a colorbar
+    cbar = plt.colorbar(image, ax=ax, ticks=[0, 1])
+    cbar.ax.set_yticklabels(['Dead', 'Alive'])
+except Exception as e:
+    print("Error during plot rendering. Error: ", str(e))
+    raise
 
 def update_image(frame, grid, image):
     """Update the image for a new frame."""
@@ -47,7 +67,15 @@ def update_image(frame, grid, image):
     return [image]
 
 # Start the animation
-ani = animation.FuncAnimation(fig, update_image, frames=1000, interval=50, blit=True, fargs=(grid, image))
+try:
+    ani = animation.FuncAnimation(fig, update_image, frames=1000, interval=50, blit=True, fargs=(grid, image))
+except Exception as e:
+    print("Error during animation setup. Error: ", str(e))
+    raise
 
 # Display the animation
-plt.show()
+try:
+    plt.show()
+except Exception as e:
+    print("Error during animation display. Error: ", str(e))
+    raise

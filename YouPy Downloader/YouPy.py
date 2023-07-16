5b3f26413bf023ac3ec487fa06125e69d2c57fd1
PortfolioAI <135471798+PortfolioAI@users.noreply.github.com>
2023-07-16 17:43:55 -0400

    Improve YouTube downloader with enhanced error handling

--- YouPy Downloader/YouPy.py
+++ YouPy Downloader/YouPy.py
@@ -3,32 +3,56 @@ from pytube.exceptions import PytubeError
 import os
 import re
 
+def get_user_input(prompt, validation_func=None):
+    while True:
+        value = input(prompt)
+        if validation_func is None or validation_func(value):
+            return value
+        print("Invalid input. Please try again.")
+
 def progress_function(stream, chunk, bytes_remaining):
     total_size = stream.filesize
     bytes_downloaded = total_size - bytes_remaining
 
-    percentage_of_completion = bytes_downloaded / total_size * 100
-    print(f"\rDownloading... {percentage_of_completion:.2f}%", end='')
+    if total_size > 0:
+        percentage_of_completion = bytes_downloaded / total_size * 100
+        print(f"\rDownloading... {percentage_of_completion:.2f}%", end='')
+    else:
+        print(f"\rDownloading... completed", end='')
+
+def get_youtube_object(url):
+    try:
+        return YouTube(url, on_progress_callback=progress_function)
+    except PytubeError as e:
+        print(f"Pytube error occurred: {type(e).__name__}, {str(e)}")
+    except Exception as e:
+        print(f"An error occurred: {type(e).__name__}, {str(e)}")
+
+def get_youtube_stream(yt, audio_only):
+    if audio_only:
+        stream = yt.streams.filter(only_audio=True).first()
+        filename_ext = '.mp3'
+    else:
+        stream = yt.streams.filter(progressive=True, file_extension='mp4').order_by('resolution').desc().first()
+        filename_ext = '.mp4'
+    if stream is None:
+        print(f"No suitable stream found for {'audio only' if audio_only else 'video'}.")
+    return stream, filename_ext
 
 def download_youtube_video(url, filename, audio_only=False):
-    try:
-        yt = YouTube(url, on_progress_callback=progress_function)
-    except PytubeError as e:
-        print(f"Pytube error occurred: {type(e).__name__}, {e.args}")
-        return
-    except Exception as e:
-        print(f"An error occurred: {type(e).__name__}, {e.args}")
+    yt = get_youtube_object(url)
+    if yt is None:
         return
 
-    if audio_only:
-        stream = yt.streams.get_audio_only()
-        filename += '.mp3'
-    else:
-        stream = yt.streams.get_highest_resolution()
-        filename += '.mp4'
+    stream, filename_ext = get_youtube_stream(yt, audio_only)
+    if stream is None:
+        return
+
+    filename += filename_ext
 
     if os.path.exists(filename):
-        overwrite = input(f"File '{filename}' already exists. Do you want to overwrite it? (yes/no): ")
+        overwrite = get_user_input(f"File '{filename}' already exists. Do you want to overwrite it? (yes/no): ", 
+                                   lambda x: x.lower() in ['yes', 'no'])
         if overwrite.lower() != 'yes':
             return
 
@@ -36,7 +60,7 @@ def download_youtube_video(url, filename, audio_only=False):
     try:
         stream.download(filename=filename)
     except Exception as e:
-        print(f"Error occurred during downloading: {type(e).__name__}, {e.args}")
+        print(f"Error occurred during downloading: {type(e).__name__}, {str(e)}")
         return
 
     print(f"\nDownloaded as {filename}")
@@ -56,22 +80,16 @@ def main():
         print("\n1. Download a YouTube video")
         print("2. Download audio from a YouTube video")
         print("3. Exit")
-        choice = input("Choose an option: ")
+        choice = get_user_input("Choose an option: ", lambda x: x in ['1', '2', '3'])
 
         if choice in ['1', '2']:
-            url = input("Enter the YouTube URL: ")
-            while not is_youtube_url(url):
-                print("Invalid YouTube URL. Please enter a valid YouTube URL.")
-                url = input("Enter the YouTube URL: ")
-
-            filename = input("Enter the filename for the downloaded content (without extension): ")
+            url = get_user_input("Enter the YouTube URL: ", is_youtube_url)
+            filename = get_user_input("Enter the filename for the downloaded content (without extension): ")
             audio_only = choice == '2'
             
             download_youtube_video(url, filename, audio_only)
         elif choice == '3':
             break
-        else:
-            print("Error: Invalid option. Please choose 1, 2, or 3.")
 
 if __name__ == "__main__":
-    main()
+    main()  
--- YouPy Downloader/readme.txt
+++ YouPy Downloader/readme.txt
@@ -1,29 +1,29 @@
-## YouPy Downloader
+# YouTube Downloader
 
-YouPy Downloader is a Python script that allows you to seamlessly download YouTube videos or just the audio from the videos with progress updates. 
+This application lets you download YouTube videos or audio files in an easy and convenient way.
+
+## Features
+
+- Download YouTube videos in highest available resolution.
+- Download only the audio from YouTube videos.
+- Informative download progress indicator.
+- Advanced error handling for robust functionality.
+
+## Usage
+
+1. Run the python script: python youtube_downloader.py
+2. Choose an option: download a video (press 1), download only audio (press 2), or exit (press 3).
+3. If you choose to download, you will be prompted to enter the YouTube URL and the filename (without extension) for the downloaded content.
+4. If the filename already exists in the directory, you will be asked if you want to overwrite it. Enter 'yes' or 'no'.
+5. The content will be downloaded to the current directory. You can check the progress in the console.
 
 ## Requirements
 
-YouPy Downloader requires Python 3 and the PyTube library. You can install PyTube with pip:
+- Python 3.x
+- Pytube library
 
-pip install pytube
-Usage
-To use YouPy Downloader, run it in your terminal or command line:
+## Installation
 
-sh
-Copy code
-python youpy_downloader.py
-Upon execution, YouPy Downloader will display a menu with three options:
-
-Download a YouTube video
-Download audio from a YouTube video
-Exit
-Enter the number of the option you want to choose.
-
-If you choose to download a video or audio, you'll be prompted to enter the YouTube URL and a filename for the downloaded content (without extension). If a file with the chosen filename already exists, you'll be asked if you want to overwrite it.
-
-The downloaded video will be in the best available quality. If you're downloading audio, only the audio track will be downloaded. The download progress will be displayed as a percentage to keep you updated.
-
-License##
-
-YouPy Downloader is intended for personal use only. Always respect copyrights and the terms of service of YouTube. Do not use this script to download copyrighted content without permission.
+1. Clone the repository
+2. Install requirements using pip: pip install -r requirements.txt
+3. Run the script: python youtube_downloader.py

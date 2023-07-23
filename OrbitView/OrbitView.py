8b4d2ea52ff165a16fd91e68025aa5055dbb313a
PortfolioAI <135471798+PortfolioAI@users.noreply.github.com>
2023-07-22 23:46:53 -0400

    Improved user-friendliness
    Improved user-friendliness and robustness of OrbitView application
    
    - Added input validation for date selection.
    - Integrated a calendar date picker for more intuitive date selection.
    - Added a 'Clear' button to allow users to reset their inputs.
    - Improved the GUI layout for a more organized view.
    - Error handling improvements to provide user-friendly error messages.
    - Display both the selected date and planet for better clarity.
    - Added README.

--- OrbitView/OrbitView.py
+++ OrbitView/OrbitView.py
@@ -1,10 +1,11 @@
 import ephem
 import matplotlib.pyplot as plt
 from tkinter import *
+from tkinter import messagebox
+from tkinter import ttk
 import numpy as np
 from datetime import datetime
-
-"OrbitView: An Interactive Solar System Explorer"
+from tkcalendar import DateEntry
 
 class App:
     def __init__(self, master):
@@ -13,33 +14,45 @@ class App:
         master.title("OrbitView: An Interactive Solar System Explorer")
 
         # List of planets to visualize
-        self.planets = ['Mercury', 'Venus', 'Mars', 'Jupiter', 'Saturn', 'Uranus', 'Neptune']
+        self.planets = ['Mercury', 'Venus', 'Earth', 'Mars', 'Jupiter', 'Saturn', 'Uranus', 'Neptune']
 
-        # Set up dropdown menu for planet selection
+        # Set up labels, dropdown menu for planet selection, date picker, and button in grid layout
+        Label(master, text="Select Planet:").grid(row=0, column=0, padx=5, pady=5, sticky='w')
+        Label(master, text="Select Date:").grid(row=1, column=0, padx=5, pady=5, sticky='w')
+
         self.planetVar = StringVar(master)
         self.planetVar.set(self.planets[0])  # default value
 
-        self.planetMenu = OptionMenu(master, self.planetVar, *self.planets)
-        self.planetMenu.pack()
+        self.planetMenu = ttk.Combobox(master, textvariable=self.planetVar, values=self.planets)
+        self.planetMenu.grid(row=0, column=1, padx=5, pady=5, sticky='w')
 
-        # Set up date input field
-        self.dateEntry = Entry(master)
-        self.dateEntry.insert(0, "yyyy/mm/dd")
-        self.dateEntry.pack()
+        self.dateEntry = DateEntry(master, date_pattern='yyyy/mm/dd')
+        self.dateEntry.grid(row=1, column=1, padx=5, pady=5, sticky='w')
 
-        # Set up button to trigger position calculation and visualization
         self.getPosButton = Button(master, text="Get Position", command=self.update_position)
-        self.getPosButton.pack()
+        self.getPosButton.grid(row=2, column=0, columnspan=2, padx=5, pady=5)
 
         # Set up label to display position information
         self.positionLabel = Label(master, text="")
-        self.positionLabel.pack()
+        self.positionLabel.grid(row=3, column=0, columnspan=2, padx=5, pady=5)
 
         # Set up scale to control viewing range (zoom level)
-        self.zoomScale = Scale(master, from_=1, to=32, orient=HORIZONTAL, length=400,
-                               label="Viewing range (AU)", sliderlength=20)
+        Label(master, text="Viewing range (AU):").grid(row=4, column=0, padx=5, pady=5, sticky='w')
+
+        self.zoomScale = Scale(master, from_=1, to=32, orient=HORIZONTAL, length=400, sliderlength=20)
         self.zoomScale.set(32)
-        self.zoomScale.pack()
+        self.zoomScale.grid(row=4, column=1, padx=5, pady=5, sticky='w')
+
+        # Add a Clear button to reset inputs
+        self.clearButton = Button(master, text="Clear", command=self.clear_inputs)
+        self.clearButton.grid(row=5, column=0, columnspan=2, padx=5, pady=5)
+
+    def clear_inputs(self):
+        # Reset the input fields to their default values
+        self.planetVar.set(self.planets[0])
+        self.dateEntry.set_date(datetime.now())
+        self.zoomScale.set(32)
+        self.positionLabel.config(text="")
 
     def update_position(self):
         # Get selected planet name and entered date
@@ -49,10 +62,10 @@ class App:
         # Update planet position and visualize
         try:
             position = self.get_planet_position(planet_name, date)
-            self.positionLabel.config(text=str(position))
+            self.positionLabel.config(text=f"Planet: {planet_name}\nDate: {date}\nPosition: {position}")
             self.plot_orbits(planet_name, self.zoomScale.get(), date)
-        except (AttributeError, ValueError):
-            self.positionLabel.config(text="Error: Invalid input. Please check the planet name and date format (yyyy/mm/dd).")
+        except (AttributeError, ValueError) as e:
+            messagebox.showerror("Error", "Invalid input. Please check the planet name and date format (yyyy/mm/dd).")
 
     @staticmethod
     def get_planet_position(planet_name, date):
---
+++ OrbitView/readme.txt
@@ -0,0 +1,29 @@
+--------------------------------------------------------------
+README - OrbitView: An Interactive Solar System Explorer
+--------------------------------------------------------------
+
+OrbitView is an interactive application developed in Python that allows users to visualize the orbits of planets in our solar system for any given date. 
+
+The application provides a simple and intuitive interface that includes a dropdown menu to select a planet, a date picker to select a date, and a scale to control the viewing range (zoom level).
+
+Usage:
+1. Run the OrbitView.py script in a Python environment with the necessary dependencies installed (tkinter, matplotlib, ephem, numpy, tkcalendar).
+2. On the application window, choose a planet from the dropdown menu.
+3. Select a date from the date picker.
+4. Adjust the viewing range using the scale.
+5. Click "Get Position" to calculate and display the position of the selected planet on the chosen date and plot the orbits of all planets.
+6. You can use the "Clear" button to reset your selections.
+
+Dependencies:
+- Python (version 3.6 or higher)
+- tkinter
+- matplotlib
+- ephem
+- numpy
+- tkcalendar
+
+To install dependencies, use pip:
+
+pip install -r requirements.txt
+
+This will install all necessary packages.

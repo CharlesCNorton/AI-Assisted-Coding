da7e8dc648e87322993574be7daf1b1c270ca4ff
PortfolioAI <135471798+PortfolioAI@users.noreply.github.com>
2023-08-15 20:50:06 -0400

    Enhance resilience & ux
    - Implement more robust URL validation using yt_dlp.
    - Add retries for failed video downloads with a max of 3 attempts.
    - Introduce filename sanitization to ensure valid filenames.
    - Offer optional custom naming for downloaded videos.
    - Streamline user interaction and provide clearer prompts.
    - Ensure compatibility with different video formats and enhance error recovery.

--- YouPy Downloader/YouPy.py
+++ YouPy Downloader/YouPy.py
@@ -2,17 +2,32 @@ import yt_dlp
 import os
 import re
 
-def validate_url(url):
-    # A basic regex to check if the URL seems like a valid YouTube URL
-    youtube_regex = re.compile(
-        r'^(https?://)?(www\.)?(youtube\.com|youtu\.?be)/.+$')
-    return youtube_regex.match(url)
+MAX_RETRIES = 3  # Max number of retries for a failed download
 
-def download_video_yt_dlp(url, output_path):
+def validate_url_using_yt_dlp(url):
+    try:
+        with yt_dlp.YoutubeDL() as ydl:
+            # Just try to extract the info, but don't download anything
+            ydl.extract_info(url, download=False)
+            return True
+    except Exception:
+        return False
+
+def download_video_yt_dlp(url, output_path, output_name=None):
+    outtmpl = f'{output_path}/'
+    if output_name:
+        outtmpl += f'{output_name}.%(ext)s'
+    else:
+        outtmpl += '%(title)s.%(ext)s'
+
     options = {
         'format': 'bestvideo+bestaudio/best',
-        'outtmpl': f'{output_path}/%(title)s.%(ext)s',
+        'outtmpl': outtmpl,
         'progress_hooks': [hook],
+        'postprocessors': [{
+            'key': 'FFmpegMetadata'
+        }],
+        'merge_output_format': 'mp4',
     }
     
     with yt_dlp.YoutubeDL(options) as ydl:
@@ -20,17 +35,21 @@ def download_video_yt_dlp(url, output_path):
 
 def hook(d):
     if d['status'] == 'downloading':
-        # Display the progress bar
         print(d['_percent_str'], end="\r")
 
+def sanitize_filename(filename):
+    return "".join([c for c in filename if c.isalpha() or c.isdigit() or c in (' ', '.', '-', '_')]).rstrip()
+
 def main():
+    print("Welcome to YouPy!!")
+    
     while True:
         # Prompt the user for the video URL
         url = input("\nEnter the YouTube video URL: ").strip()
 
         # Validate the URL
-        if not validate_url(url):
-            print("Invalid YouTube URL. Please try again.")
+        if not validate_url_using_yt_dlp(url):
+            print("Invalid YouTube URL or video might be restricted. Please try again.")
             continue
 
         # Prompt the user for the desired output path
@@ -45,12 +64,26 @@ def main():
                 print("Please provide a valid directory.")
                 continue
 
+        # Ask the user for a custom output name (optional)
+        output_name = input("Enter a name for the output file (Leave empty for the video's title): ").strip()
+        output_name = sanitize_filename(output_name)
+
         # Attempt to download the video
-        try:
-            download_video_yt_dlp(url, output_path)
-            print("\nVideo downloaded successfully!")
-        except Exception as e:
-            print(f"\nAn error occurred: {str(e)}")
+        retries = 0
+        while retries < MAX_RETRIES:
+            try:
+                download_video_yt_dlp(url, output_path, output_name)
+                print("\nVideo downloaded successfully!")
+                break
+            except Exception as e:
+                retries += 1
+                print(f"\nAn error occurred: {str(e)}")
+                if retries < MAX_RETRIES:
+                    choice = input(f"Do you want to retry? ({MAX_RETRIES - retries} retries left) (yes/no): ").strip().lower()
+                    if choice != 'yes':
+                        break
+                else:
+                    print("Max retries reached. Moving on.")
 
         # Ask the user if they want to download another video or exit
         choice = input("\nDo you want to download another video? (yes/no): ").strip().lower()
@@ -59,5 +92,4 @@ def main():
             break
 
 if __name__ == "__main__":
-    print("Welcome to YouPy!!")
     main()

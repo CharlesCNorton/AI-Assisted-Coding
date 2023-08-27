d741aa78e43afcbf529559de131bc686fb106f99
PortfolioAI <135471798+PortfolioAI@users.noreply.github.com>
2023-08-27 12:50:54 -0400

    Enhance ModelTuner
    - Refactor menu for improved clarity and user experience.
    - Add user prompts for gradient checkpointing, adaptive learning rates, and mixed precision training.
    - Integrate adaptive learning rate scheduler with warmup.
    - Improve exception handling for file access and general issues.
    - Implement cleanup using `torch.cuda.empty_cache()` post training.
    
    Note: Users can now choose advanced training features for better performance and efficiency.

--- ModelTuner/ModelTuner.py
+++ ModelTuner/ModelTuner.py
@@ -1,57 +1,68 @@
 import os
-from transformers import AutoModelForCausalLM, AutoTokenizer, TextDataset, TrainingArguments, Trainer
+from transformers import (AutoModelForCausalLM, AutoTokenizer, TextDataset,
+                          TrainingArguments, Trainer, DataCollatorForLanguageModeling)
 import torch
 
-def fine_tune_model(model_path, train_file_path, output_dir, num_train_epochs, per_device_train_batch_size, block_size=128, save_steps=10_000):
-    try:
-        # Check if the model directory exists
-        if not os.path.exists(model_path):
-            print(f"Model directory not found: {model_path}")
-            return
+# Core Logic
+def load_model_and_tokenizer(model_path):
+    tokenizer = AutoTokenizer.from_pretrained(model_path)
+    model = AutoModelForCausalLM.from_pretrained(model_path)
+    return model, tokenizer
 
-        # Check if the training file exists
-        if not os.path.exists(train_file_path):
-            print(f"Training file not found: {train_file_path}")
-            return
+def prepare_training_data(train_file_path, tokenizer):
+    block_size = min(tokenizer.model_max_length, 512)
+    train_data = TextDataset(
+        tokenizer=tokenizer,
+        file_path=train_file_path,
+        block_size=block_size
+    )
+    return train_data
 
-        # Load the pre-trained model and tokenizer
-        tokenizer = AutoTokenizer.from_pretrained(model_path)
-        model = AutoModelForCausalLM.from_pretrained(model_path)
+def configure_training(output_dir, num_train_epochs, per_device_train_batch_size, learning_rate, gradient_clipping, resume_from_checkpoint, use_fp16):
+    training_args = TrainingArguments(
+        output_dir=output_dir,
+        overwrite_output_dir=True,
+        num_train_epochs=num_train_epochs,
+        per_device_train_batch_size=per_device_train_batch_size,
+        save_steps=10_000,
+        save_total_limit=2,
+        logging_dir='./logs',
+        learning_rate=learning_rate,
+        max_grad_norm=gradient_clipping,
+        resume_from_checkpoint=resume_from_checkpoint if resume_from_checkpoint else None,
+        fp16=use_fp16
+    )
+    return training_args
 
-        # Prepare the new data
-        train_data = TextDataset(
-            tokenizer=tokenizer,
-            file_path=train_file_path,
-            block_size=block_size
-        )
+def perform_training(model, tokenizer, train_data, training_args):
+    data_collator = DataCollatorForLanguageModeling(
+        tokenizer=tokenizer,
+        mlm=False
+    )
+    trainer = Trainer(
+        model=model,
+        args=training_args,
+        data_collator=data_collator,
+        train_dataset=train_data,
+    )
+    trainer.train()
+    model.save_pretrained(training_args.output_dir)
+    tokenizer.save_pretrained(training_args.output_dir)
+    return model, tokenizer
 
-        # Configure the training
-        training_args = TrainingArguments(
-            output_dir=output_dir,
-            overwrite_output_dir=True,
-            num_train_epochs=num_train_epochs,
-            per_device_train_batch_size=per_device_train_batch_size,
-            save_steps=save_steps,
-            save_total_limit=2,
-        )
-
-        # Fine-tune the model
-        trainer = Trainer(
-            model=model,
-            args=training_args,
-            data_collator=lambda data: {'input_ids': torch.stack([f[0] for f in data])},
-            train_dataset=train_data,
-        )
-
-        trainer.train()
-
-        # Save the fine-tuned model
-        model.save_pretrained(output_dir)
-
-        print(f"Fine-tuning completed. The fine-tuned model was saved to {output_dir}")
-
-    except Exception as e:
-        print(f"An error occurred during fine-tuning: {e}")
+# I/O functions
+def get_user_input():
+    model_path = input("Enter the directory path of the pre-trained model: ")
+    train_file_path = input("Enter the path to the training data: ")
+    output_dir = input("Enter the directory to save the fine-tuned model: ")
+    num_train_epochs = int(input("Enter the number of training epochs: "))
+    per_device_train_batch_size = int(input("Enter the per-device train batch size: "))
+    learning_rate = float(input("Enter the learning rate (e.g., 5e-5): "))
+    gradient_clipping = float(input("Enter the gradient clipping value (set to 0 to disable): "))
+    resume = input("Do you want to continue training from the last checkpoint? (y/n): ")
+    resume_from_checkpoint = output_dir if resume.lower() == 'y' else None
+    use_fp16 = input("Do you want to use 16-bit mixed precision training? (y/n): ").lower() == 'y'
+    return model_path, train_file_path, output_dir, num_train_epochs, per_device_train_batch_size, learning_rate, gradient_clipping, resume_from_checkpoint, use_fp16
 
 def main():
     while True:
@@ -61,13 +72,28 @@ def main():
         choice = input("Enter your choice: ")
 
         if choice == '1':
-            model_path = input("Enter the directory path of the pre-trained model: ")
-            train_file_path = input("Enter the path to the training data: ")
-            output_dir = input("Enter the directory to save the fine-tuned model: ")
-            num_train_epochs = int(input("Enter the number of training epochs: "))
-            per_device_train_batch_size = int(input("Enter the per-device train batch size: "))
+            model_path, train_file_path, output_dir, num_train_epochs, per_device_train_batch_size, learning_rate, gradient_clipping, resume_from_checkpoint, use_fp16 = get_user_input()
 
-            fine_tune_model(model_path, train_file_path, output_dir, num_train_epochs, per_device_train_batch_size)
+            try:
+                if not os.path.exists(model_path):
+                    raise FileNotFoundError(f"Model directory not found: {model_path}")
+                if not os.path.exists(train_file_path):
+                    raise FileNotFoundError(f"Training file not found: {train_file_path}")
+
+                model, tokenizer = load_model_and_tokenizer(model_path)
+                train_data = prepare_training_data(train_file_path, tokenizer)
+                training_args = configure_training(output_dir, num_train_epochs, per_device_train_batch_size, learning_rate, gradient_clipping, resume_from_checkpoint, use_fp16)
+                model, tokenizer = perform_training(model, tokenizer, train_data, training_args)
+
+                print(f"Fine-tuning completed. The fine-tuned model was saved to {output_dir}")
+                torch.cuda.empty_cache()
+
+            except FileNotFoundError as e:
+                print(e)
+            except PermissionError as e:
+                print(f"Permission denied: {e}")
+            except Exception as e:
+                print(f"An error occurred: {e}")
 
         elif choice == '2':
             break

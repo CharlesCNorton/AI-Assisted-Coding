1a2bdf16f8d1af217de39cfdf81d5833ebb7814c
PortfolioAI <135471798+PortfolioAI@users.noreply.github.com>
2023-08-30 11:57:14 -0400

    : Enhance Fine-tuning CLI with Visual Enhancements and Parameter Guidelines
    - Integrated the `colorama` module for terminal coloration to improve visual differentiation in the fine-tuning CLI.
    - Added suggested value ranges and reasons for each configuration parameter in the `TOOLTIPS` dictionary.
    - Provided color-coded guidance and error messages to enhance user experience.
    - Improved user prompts with better contextual information.
    
    This enhancement aims to provide users with clearer visual cues and guidelines when fine-tuning models, ensuring an informed and user-friendly experience.

--- ModelTuner/ModelTuner.py
+++ ModelTuner/ModelTuner.py
@@ -4,129 +4,129 @@ from transformers import (
     DataCollatorForLanguageModeling, get_constant_schedule_with_warmup
 )
 import torch
+from collections import namedtuple
+from colorama import init, Fore, Back, Style
 
-# Global Default Configurations
-CONFIG = {
-    "model_path": "path_to_model",
-    "train_file_path": "path_to_data",
-    "output_dir": "output_directory",
-    "num_train_epochs": 1,
-    "per_device_train_batch_size": 2,
-    "learning_rate": 5e-5,
-    "gradient_clipping": 1.0,
-    "resume_from_checkpoint": None,
-    "use_fp16": False,
-    "use_adaptive_lr": False,
-    "warmup_steps": 0
+# Initialize colorama
+init(autoreset=True)
+
+# Configuration namedtuple for better organization
+Config = namedtuple('Config', [
+    "model_path",
+    "train_file_path",
+    "output_dir",
+    "num_train_epochs",
+    "per_device_train_batch_size",
+    "learning_rate",
+    "gradient_clipping",
+    "resume_from_checkpoint",
+    "use_fp16",
+    "use_adaptive_lr",
+    "warmup_steps"
+])
+
+# Default Configurations
+DEFAULT_CONFIG = Config(
+    model_path="gpt2-medium",
+    train_file_path="path_to_data",
+    output_dir="output_directory",
+    num_train_epochs=1,
+    per_device_train_batch_size=2,
+    learning_rate=5e-5,
+    gradient_clipping=1.0,
+    resume_from_checkpoint=None,
+    use_fp16=False,
+    use_adaptive_lr=False,
+    warmup_steps=0
+)
+
+# Tooltips for each configuration parameter
+TOOLTIPS = {
+    "model_path": "Path to the pre-trained model or model identifier from huggingface.co/models.",
+    "train_file_path": "Path to the training data.",
+    "output_dir": "Path to save the fine-tuned model.",
+    "num_train_epochs": "Number of training epochs. Suggested range: 1-10, depending on data size.",
+    "per_device_train_batch_size": "Batch size per device during training. Suggested range: 2-32, based on GPU memory.",
+    "learning_rate": "Training learning rate. Suggested range: 1e-6 to 1e-3. Smaller datasets might require smaller LR.",
+    "gradient_clipping": "Maximum norm of the gradients. Suggested range: 0.1-5.0. Prevents exploding gradients.",
+    "resume_from_checkpoint": "Path to resume training from a checkpoint.",
+    "use_fp16": "Use 16-bit (mixed) precision training instead of 32-bit. Can speed up training on supported GPUs.",
+    "use_adaptive_lr": "Use adaptive learning rate with warmup steps. Adapts learning rate during training.",
+    "warmup_steps": "Number of warmup steps for adaptive learning rate. Suggested range: 0-5000. Helps in stabilizing training in the beginning."
 }
 
-# Core Logic
-def load_model_and_tokenizer(model_path):
-    tokenizer = AutoTokenizer.from_pretrained(model_path)
-    model = AutoModelForCausalLM.from_pretrained(model_path)
-    return model, tokenizer
+def set_config_parameter(config):
+    """Modify a configuration parameter."""
+    fields = config._fields
+    print(Fore.BLUE + "\nChoose parameter to modify:")
+    for idx, field in enumerate(fields):
+        print(Fore.GREEN + f"{idx + 1}. {field} (Current: {getattr(config, field)})")
+        print(Fore.WHITE + f"   - {TOOLTIPS[field]}")
 
-def prepare_training_data(train_file_path, tokenizer):
-    block_size = min(tokenizer.model_max_length, 512)
-    train_data = TextDataset(
-        tokenizer=tokenizer,
-        file_path=train_file_path,
-        block_size=block_size
-    )
-    return train_data
+    choice = int(input(Fore.YELLOW + "Your choice: ")) - 1
+    key = fields[choice]
+    current_value = getattr(config, key)
 
-def configure_training(output_dir, num_train_epochs, per_device_train_batch_size, learning_rate, gradient_clipping, resume_from_checkpoint, use_fp16, use_adaptive_lr, warmup_steps):
-    training_args = TrainingArguments(
-        output_dir=output_dir,
-        overwrite_output_dir=True,
-        num_train_epochs=num_train_epochs,
-        per_device_train_batch_size=per_device_train_batch_size,
-        save_steps=10_000,
-        save_total_limit=2,
-        logging_dir='./logs',
-        learning_rate=learning_rate,
-        max_grad_norm=gradient_clipping,
-        resume_from_checkpoint=resume_from_checkpoint if resume_from_checkpoint else None,
-        fp16=use_fp16
-    )
-    scheduler = get_constant_schedule_with_warmup(training_args.optimizer, num_warmup_steps=warmup_steps) if use_adaptive_lr else None
-    return training_args, scheduler
+    # For boolean values, provide a toggle option
+    if isinstance(current_value, bool):
+        new_value = input(f"Do you want to enable {key}? (yes/no, Current: {current_value}): ").lower() == 'yes'
+    else:
+        new_value = input(f"Enter new value for {key} (Press Enter to keep current value: {current_value}): ")
+        if not new_value:
+            new_value = current_value
+        else:
+            # Convert the input to the appropriate type
+            if isinstance(current_value, int):
+                new_value = int(new_value)
+            elif isinstance(current_value, float):
+                new_value = float(new_value)
 
-def perform_training(model, tokenizer, train_data, training_args, scheduler=None):
-    data_collator = DataCollatorForLanguageModeling(
-        tokenizer=tokenizer,
-        mlm=False
-    )
-    trainer = Trainer(
-        model=model,
-        args=training_args,
-        data_collator=data_collator,
-        train_dataset=train_data,
-        lr_scheduler=scheduler
-    )
-    trainer.train()
-    model.save_pretrained(training_args.output_dir)
-    tokenizer.save_pretrained(training_args.output_dir)
-    return model, tokenizer
-
-# Configuration Functions
-def view_config():
-    for key, value in CONFIG.items():
-        print(f"{key}: {value}")
-    input("\nPress any key to continue...")
-
-def set_config_parameter():
-    print("\nChoose parameter to modify:")
-    for idx, key in enumerate(CONFIG.keys()):
-        print(f"{idx + 1}. {key}")
-    choice = int(input("Your choice: ")) - 1
-    key = list(CONFIG.keys())[choice]
-    if isinstance(CONFIG[key], str):
-        CONFIG[key] = input(f"Enter new value for {key}: ")
-    elif isinstance(CONFIG[key], int):
-        CONFIG[key] = int(input(f"Enter new value for {key}: "))
-    elif isinstance(CONFIG[key], float):
-        CONFIG[key] = float(input(f"Enter new value for {key}: "))
-    elif isinstance(CONFIG[key], bool):
-        CONFIG[key] = input(f"Enter new value for {key} (True/False): ").lower() == 'true'
-    print(f"{key} updated to {CONFIG[key]}")
+    updated_config = config._replace(**{key: new_value})
+    print(Fore.CYAN + f"{key} updated to {getattr(updated_config, key)}")
+    return updated_config
 
 def main():
+    config = DEFAULT_CONFIG
+
     while True:
-        print("\n===== Fine-tuning Menu =====")
-        print("1. Fine-tune a model with current configuration")
-        print("2. View current configuration")
-        print("3. Modify a configuration parameter")
-        print("4. Exit")
-        choice = input("Your choice: ")
+        print(Fore.BLUE + "\n===== Fine-tuning Menu =====")
+        print(Fore.GREEN + "1. Fine-tune a model with current configuration")
+        print(Fore.GREEN + "2. View current configuration")
+        print(Fore.GREEN + "3. Modify a configuration parameter")
+        print(Fore.GREEN + "4. Exit")
+        choice = input(Fore.YELLOW + "Your choice: ")
 
         if choice == '1':
             try:
-                model, tokenizer = load_model_and_tokenizer(CONFIG["model_path"])
-                train_data = prepare_training_data(CONFIG["train_file_path"], tokenizer)
-                training_args, scheduler = configure_training(CONFIG["output_dir"], CONFIG["num_train_epochs"], CONFIG["per_device_train_batch_size"], CONFIG["learning_rate"], CONFIG["gradient_clipping"], CONFIG["resume_from_checkpoint"], CONFIG["use_fp16"], CONFIG["use_adaptive_lr"], CONFIG["warmup_steps"])
+                model, tokenizer = load_model_and_tokenizer(config.model_path)
+                train_data = prepare_training_data(config.train_file_path, tokenizer)
+                training_args, scheduler = configure_training(config)
                 model, tokenizer = perform_training(model, tokenizer, train_data, training_args, scheduler)
-                print(f"Fine-tuning completed. Saved to {CONFIG['output_dir']}")
-                torch.cuda.empty_cache()
+                print(Fore.CYAN + f"Fine-tuning completed. Saved to {config.output_dir}")
+
+                # Clear GPU memory if available
+                if torch.cuda.is_available():
+                    torch.cuda.empty_cache()
 
             except FileNotFoundError as e:
-                print(e)
+                print(Fore.RED + str(e))
             except PermissionError as e:
-                print(f"Permission denied: {e}")
+                print(Fore.RED + f"Permission denied: {e}")
             except Exception as e:
-                print(f"An error occurred: {e}")
+                print(Fore.RED + f"An error occurred: {e}")
 
         elif choice == '2':
-            view_config()
+            view_config(config)
 
         elif choice == '3':
-            set_config_parameter()
+            config = set_config_parameter(config)
 
         elif choice == '4':
             break
 
         else:
-            print("Invalid choice. Please select a valid option.")
+            print(Fore.RED + "Invalid choice. Please select a valid option.")
+
 
 if __name__ == "__main__":
     main()

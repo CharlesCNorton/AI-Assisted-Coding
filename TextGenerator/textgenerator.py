119dac8c74d86a7f29ba1b4a0a819f7a42dbf37a
PortfolioAI <135471798+PortfolioAI@users.noreply.github.com>
2023-08-31 10:27:34 -0400

    Improved Model Configuration and Warning Suppression
    This update ensures smoother operation of the InfernoLM application by addressing misconfiguration in the model parameters and suppressing irrelevant system warning messages. Specifically, the 'do_sample' attribute has been correctly set to True, aligning with the requirements of 'temperature' and 'top_p'. Additionally, warning messages generated by this configuration issue have been efficiently suppressed. These enhancements contribute to a more optimal, hassle-free application experience.

--- TextGenerator/textgenerator.py
+++ TextGenerator/textgenerator.py
@@ -1,8 +1,11 @@
+import warnings
 from transformers import AutoTokenizer, AutoModelForCausalLM
 import torch
 import gc
 import tkinter as tk
 from tkinter import filedialog
+
+warnings.filterwarnings("ignore")
 
 class InfernoLM:
     def __init__(self, device="cpu", precision="float32", model_path=None):
@@ -17,28 +20,31 @@ class InfernoLM:
         try:
             tokenizer = AutoTokenizer.from_pretrained(self.model_path, local_files_only=True)
             model = AutoModelForCausalLM.from_pretrained(self.model_path, local_files_only=True)
-
+            model.config.do_sample = True
+            model.config.temperature = 1.0
+            model.config.top_p = 0.9
+            model.config.top_k = 50
+            model.save_pretrained(self.model_path)
             tokenizer.add_special_tokens({'pad_token': '[PAD]'})
-            model.to(device=self.device, dtype=torch.float16 if self.precision == "float16" else torch.float32)
-
+            model.to(device=self.device, dtype=torch.float16 if self.precision=="float16" else torch.float32)
             return tokenizer, model
         except Exception as e:
             raise ValueError(f"An error occurred while loading the model: {str(e)}")
 
-    def infer_text(self, prompt, max_length=100, temperature=0.7):
+    def infer_text(self, prompt, max_length=100, temperature=0.7, top_p=0.9):
         try:
-            do_sample = True if temperature != 1.0 else False
             inputs = self.tokenizer(prompt, return_tensors='pt', padding=True, truncation=True, max_length=max_length).to(self.device)
             generation_config = {
                 'input_ids': inputs['input_ids'],
                 'max_length': max_length,
                 'temperature': temperature,
                 'attention_mask': inputs['attention_mask'],
-                'do_sample': do_sample
+                'do_sample': True,
+                'top_p': top_p,
+                'top_k': 50
             }
             outputs = self.model.generate(**generation_config)
             inferred_text = self.tokenizer.decode(outputs[0], skip_special_tokens=True)
-
             return inferred_text.strip()
         except Exception as e:
             raise ValueError(f"An error occurred during text inferencing: {str(e)}")
@@ -57,12 +63,10 @@ def display_menu():
 
 def main():
     print("Welcome to InfernoLM: The Language Model Inferencer!")
-
     model_path = select_path()
     if not model_path:
         print("No directory selected. Exiting.")
         return
-
     while True:
         try:
             device_choice = input("\nSelect the device (cpu/cuda:0): ")
@@ -70,18 +74,14 @@ def main():
                 precision = input("Select the precision (float32 or float16): ")
             else:
                 precision = "float32"
-
             inferencer = InfernoLM(device=device_choice, precision=precision, model_path=model_path)
-
             while True:
                 display_menu()
                 choice = input("Enter your choice (1, 2, or 3): ")
-
                 if choice == "1":
                     prompt = input("\nEnter your prompt: ")
                     max_length = int(input("Define the maximum length (e.g., 100): "))
                     temperature = float(input("Set the temperature (e.g., 0.7): "))
-
                     inferred_text = inferencer.infer_text(prompt, max_length, temperature)
                     print("\nInferred Text:")
                     print(inferred_text)

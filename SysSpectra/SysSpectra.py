272dba8e4a3a77320294c068b476002121d404cc
PortfolioAI <135471798+PortfolioAI@users.noreply.github.com>
2023-09-01 22:12:41 -0400

    Refactor and enhance SysSpectra.py
    - Organize and group imports for better readability.
    - Introduce docstrings to classes and methods.
    - Refactor UI initialization for clarity and maintainability.
    - Implement specific exception handling for GPU information.
    - Use constants for fixed values such as refresh rate.

--- SysSpectra/SysSpectra.py
+++ SysSpectra/SysSpectra.py
@@ -2,63 +2,77 @@ import psutil
 from GPUtil import getGPUs
 from tkinter import Tk, RIGHT, BOTH, X, Button, ttk
 
+REFRESH_RATE = 1000  # Refresh rate in milliseconds
+
 class SysInfo(ttk.Frame):
-    def __init__(self):
-        super().__init__()
-        self.initUI()
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
         self.style = ttk.Style()
-        self.style.configure("BW.TFrame", background="white")
+        self.night_mode = False
+        self.initUI()
+        self.update_info()
 
     def initUI(self):
         self.master.title("System Info")
         self.pack(fill=BOTH, expand=True)
 
-        frame1 = ttk.Frame(self, style="BW.TFrame")
-        frame1.pack(fill=X)
-        vram_label = ttk.Label(frame1, text="VRAM: ", justify=RIGHT)
-        vram_label.pack(side=RIGHT, padx=5, pady=5)
+        self.initFrames()
+        self.initLabels()
+        self.initButtons()
 
-        frame2 = ttk.Frame(self, style="BW.TFrame")
-        frame2.pack(fill=X)
-        ram_label = ttk.Label(frame2, text="RAM: ", justify=RIGHT)
-        ram_label.pack(side=RIGHT, padx=5, pady=5)
+    def initFrames(self):
+        self.frame1 = ttk.Frame(self, style="BW.TFrame")
+        self.frame1.pack(fill=X)
 
-        self.night_mode = False
-        self.vram_label = vram_label
-        self.ram_label = ram_label
-        night_mode_frame = ttk.Frame(self, style="BW.TFrame")
-        night_mode_frame.pack(fill=X)
+        self.frame2 = ttk.Frame(self, style="BW.TFrame")
+        self.frame2.pack(fill=X)
 
-        self.night_mode_btn = Button(night_mode_frame, text="Night Mode", command=self.toggle_night_mode)
+        self.night_mode_frame = ttk.Frame(self, style="BW.TFrame")
+        self.night_mode_frame.pack(fill=X)
+
+    def initLabels(self):
+        self.vram_label = ttk.Label(self.frame1, text="VRAM: ", justify=RIGHT)
+        self.vram_label.pack(side=RIGHT, padx=5, pady=5)
+
+        self.ram_label = ttk.Label(self.frame2, text="RAM: ", justify=RIGHT)
+        self.ram_label.pack(side=RIGHT, padx=5, pady=5)
+
+    def initButtons(self):
+        self.night_mode_btn = Button(self.night_mode_frame, text="Night Mode", command=self.toggle_night_mode)
         self.night_mode_btn.pack(side=RIGHT, padx=5, pady=5)
-        self.update_info()
 
     def toggle_night_mode(self):
         if self.night_mode:
-            self.style.configure("BW.TFrame", background="white")
-            self.vram_label.config(background="white", foreground="black")
-            self.ram_label.config(background="white", foreground="black")
-            self.night_mode_btn.config(text="Night Mode")
+            self.set_theme("white", "black", "Night Mode")
         else:
-            self.style.configure("BW.TFrame", background="black")
-            self.vram_label.config(background="black", foreground="white")
-            self.ram_label.config(background="black", foreground="white")
-            self.night_mode_btn.config(text="Day Mode")
+            self.set_theme("black", "white", "Day Mode")
         self.night_mode = not self.night_mode
 
+    def set_theme(self, bg_color, fg_color, btn_text):
+        self.style.configure("BW.TFrame", background=bg_color)
+        self.vram_label.config(background=bg_color, foreground=fg_color)
+        self.ram_label.config(background=bg_color, foreground=fg_color)
+        self.night_mode_btn.config(text=btn_text)
+
     def update_info(self):
+        # Fetch GPU information
         try:
             GPUs = getGPUs()
             gpu = GPUs[0]
             vram_perc = round(gpu.memoryUsed / gpu.memoryTotal * 100, 2)
             self.vram_label.config(text=f"VRAM: {vram_perc}%")
+        except IndexError:
+            self.vram_label.config(text="VRAM: GPU not found")
         except Exception as e:
             self.vram_label.config(text=f"VRAM: error {str(e)}")
 
+        # Fetch RAM information
         ram = psutil.virtual_memory()
         ram_perc = round(ram.used / ram.total * 100, 2)
         self.ram_label.config(text=f"RAM: {ram_perc}%")
-        self.after(1000, self.update_info)
+
+        self.after(REFRESH_RATE, self.update_info)
 
 def main():
     root = Tk()

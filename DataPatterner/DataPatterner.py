6b823017344232ac790d86254164e18dfd7c102c
PortfolioAI <135471798+PortfolioAI@users.noreply.github.com>
2023-09-12 23:21:21 -0400

    Refactor Data Parser for Enhanced Usability and Efficiency
    - Modularized main menu options into individual functions for better code organization.
    - Improved user prompts with clearer messages and validations.
    - Optimized code for reduced redundancy in file path and regex pattern prompts.
    - Enclosed filenames in quotes for better visibility in prompts.

--- DataPatterner/DataPatterner.py
+++ DataPatterner/DataPatterner.py
@@ -7,7 +7,7 @@ def prompt_file_path(message):
         file_path = input(message)
         if os.path.exists(file_path):
             return file_path
-        print(f"File {file_path} not found.")
+        print(f"File '{file_path}' not found. Please enter a valid path.")
 
 def prompt_for_regex():
     """Prompts the user for a regex pattern."""
@@ -30,14 +30,14 @@ def read_input_file(file_path):
 
 def write_to_output_file(file_path, data):
     """Writes parsed data to the output file, with overwrite confirmation."""
-    if os.path.exists(file_path) and input(f"{file_path} already exists. Do you want to overwrite? (y/n): ").lower() != 'y':
+    if os.path.exists(file_path) and input(f"'{file_path}' already exists. Do you want to overwrite? (y/n): ").lower() != 'y':
         print("Operation aborted.")
         return
 
     try:
         with open(file_path, 'w', encoding='utf-8') as file:
             file.writelines(f"{line}\n" for line in data)
-        print(f"Data written to {file_path}")
+        print(f"Data written to '{file_path}'")
     except Exception as e:
         print(f"Error writing to file: {e}")
 
@@ -52,6 +52,27 @@ def test_pattern_on_sample(content, regex_pattern):
         print(f"Original Line: {sample.strip()}")
         print(f"Matched: {match.group(0)}\n" if match else "No Match\n")
 
+def process_parsing():
+    input_file = prompt_file_path("Enter the path to the input data file: ")
+    output_file = input("Enter the path to the output file: ")
+    regex_pattern = prompt_for_regex()
+
+    content = read_input_file(input_file)
+    parsed_data = parse_data(content, regex_pattern)
+
+    if not parsed_data:
+        print("No matches found using the provided regex pattern.")
+        return
+
+    write_to_output_file(output_file, parsed_data)
+
+def test_pattern():
+    input_file = prompt_file_path("Enter the path to the sample data file: ")
+    regex_pattern = prompt_for_regex()
+
+    content = read_input_file(input_file)
+    test_pattern_on_sample(content, regex_pattern)
+
 def main():
     print("Welcome to the Data Parser!")
 
@@ -61,32 +82,14 @@ def main():
         print("2. Test Pattern on Sample Data")
         print("3. Exit")
 
-        choice = input("\nEnter your choice: ")
+        choice = input("\nEnter your choice (1-3): ")
 
         if choice == '1':
-            input_file = prompt_file_path("Enter the path to the input data file: ")
-            output_file = input("Enter the path to the output file: ")
-            regex_pattern = prompt_for_regex()
-
-            content = read_input_file(input_file)
-            parsed_data = parse_data(content, regex_pattern)
-
-            if not parsed_data:
-                print("No matches found using the provided regex pattern.")
-                continue
-
-            write_to_output_file(output_file, parsed_data)
-
+            process_parsing()
         elif choice == '2':
-            input_file = prompt_file_path("Enter the path to the sample data file: ")
-            regex_pattern = prompt_for_regex()
-
-            content = read_input_file(input_file)
-            test_pattern_on_sample(content, regex_pattern)
-
+            test_pattern()
         elif choice == '3':
             break
-
         else:
             print("Invalid choice. Please select from the given options.")
 

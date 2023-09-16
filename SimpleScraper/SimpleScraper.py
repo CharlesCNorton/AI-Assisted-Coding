186b9070c23a96b2f0804b689672996603e78be5
PortfolioAI <135471798+PortfolioAI@users.noreply.github.com>
2023-09-15 21:39:31 -0400

    Refactor SimpleScraper for Better User Experience and Maintainability
    - Consolidated error messages into a centralized dictionary for better manageability.
    - Enhanced URL validation logic, allowing users to re-enter a URL after an error.
    - Renamed functions for better clarity and readability.
    - Streamlined user prompts using modular utility functions.
    - Improved website rendering logic, making it more concise.

--- SimpleScraper/SimpleScraper.py
+++ SimpleScraper/SimpleScraper.py
@@ -5,39 +5,36 @@ Welcome to SimpleScraper!
 import requests
 from bs4 import BeautifulSoup
 
-def get_website_content(url):
+ERROR_MESSAGES = {
+    "Timeout": "The request timed out. Please check your network connection or try again later.",
+    "TooManyRedirects": "The request exceeded the configured number of maximum redirections.",
+    "SSLError": "A SSL error occurred.",
+    "RequestException": "An error occurred while trying to fetch the website."
+}
+
+def fetch_website_content(url):
     """Fetch the website content"""
     try:
         response = requests.get(url, timeout=5)
         response.raise_for_status()
         return response.content
-    except requests.exceptions.Timeout:
-        print("The request timed out. Please check your network connection or try again later.")
-    except requests.exceptions.TooManyRedirects:
-        print("The request exceeded the configured number of maximum redirections.")
-    except requests.exceptions.SSLError:
-        print("A SSL error occurred.")
     except requests.exceptions.RequestException as e:
-        print(f"An error occurred while trying to fetch the website: {e}")
-    return None
+        error_type = type(e).__name__
+        print(ERROR_MESSAGES.get(error_type, f"An error occurred: {e}"))
+        return None
 
 def render_website(content, line_numbers=False, line_length=None):
     """Parse and print the website content"""
     soup = BeautifulSoup(content, "html.parser")
-    text = soup.get_text()
-    text = text.replace('&lt;', '<').replace('&gt;', '>')
-    text = text.strip()
+    text = soup.get_text().strip()
     lines = text.split('\n')
     for i, line in enumerate(lines):
         if line:
-            if line_numbers:
-                print(f"{i+1: >3}: ", end="")
-            if line_length:
-                print(line[:line_length].rstrip())
-            else:
-                print(line.rstrip())
+            prefix = f"{i+1: >3}: " if line_numbers else ""
+            line_content = line[:line_length].rstrip() if line_length else line.rstrip()
+            print(f"{prefix}{line_content}")
 
-def get_url():
+def prompt_url():
     """Prompt user for URL and validate it"""
     while True:
         url = input("Enter the URL of the website (including http or https) or 'q' to quit: ")
@@ -48,16 +45,18 @@ def get_url():
         else:
             print("Invalid URL. Please try again.")
 
-def get_line_numbers():
-    """Prompt user for line numbers and validate input"""
+def prompt_for_boolean(message, true_option='y', false_option='n'):
+    """Prompt user for a boolean choice and validate input"""
     while True:
-        choice = input("Show line numbers? (y/n): ").lower()
-        if choice in ['y', 'n']:
-            return choice == 'y'
+        choice = input(message).lower()
+        if choice == true_option:
+            return True
+        elif choice == false_option:
+            return False
         else:
-            print("Invalid input. Please enter 'y' for yes or 'n' for no.")
+            print(f"Invalid input. Please enter '{true_option}' or '{false_option}'.")
 
-def get_line_length():
+def prompt_line_length():
     """Prompt user for line length and validate it"""
     while True:
         line_length = input("Maximum line length (leave blank for no limit): ")
@@ -70,12 +69,12 @@ def get_line_length():
 
 def main():
     while True:
-        url = get_url()
+        url = prompt_url()
         if url is None:
             break
-        line_numbers = get_line_numbers()
-        line_length = get_line_length()
-        content = get_website_content(url)
+        line_numbers = prompt_for_boolean("Show line numbers? (y/n): ")
+        line_length = prompt_line_length()
+        content = fetch_website_content(url)
         if content is not None:
             render_website(content, line_numbers=line_numbers, line_length=line_length)
 

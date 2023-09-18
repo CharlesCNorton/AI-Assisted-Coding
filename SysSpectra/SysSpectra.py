25884835ef0cbcba8af68a1a57700eba1d0fa32e
PortfolioAI <135471798+PortfolioAI@users.noreply.github.com>
2023-09-18 18:09:21 -0400

    Multiple improvements
    - Switch from `ttk` to standard `tkinter` widgets for elements that need direct color configuration.
    - Introduced a theme dictionary to manage day/night mode styles.
    - Fixed Night Mode functionality to adjust background and foreground colors of all UI elements.
    - Centralized the application window position on the screen.
    - Improved code readability and structure.

--- SysSpectra/SysSpectra.py
+++ SysSpectra/SysSpectra.py
@@ -1,17 +1,29 @@
 import psutil
 from GPUtil import getGPUs
-from tkinter import Tk, RIGHT, BOTH, X, Button, ttk
+from tkinter import Tk, RIGHT, BOTH, X, Button, Frame, Label
 
 REFRESH_RATE = 1000
 
-class SysInfo(ttk.Frame):
+class SysInfo(Frame):
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.style = ttk.Style()
         self.night_mode = False
+        self.themes = {
+            "day": {
+                "bg": "white",
+                "fg": "black",
+                "btn_text": "Night Mode"
+            },
+            "night": {
+                "bg": "black",
+                "fg": "white",
+                "btn_text": "Day Mode"
+            }
+        }
         self.initUI()
         self.update_info()
+        self.set_theme("day")
 
     def initUI(self):
         self.master.title("System Info")
@@ -22,52 +34,60 @@ class SysInfo(ttk.Frame):
         self.initButtons()
 
     def initFrames(self):
-        self.frame1 = ttk.Frame(self, style="BW.TFrame")
-        self.frame1.pack(fill=X)
+        self.frame1 = Frame(self)
+        self.frame1.pack(fill=X, padx=10, pady=5)
 
-        self.frame2 = ttk.Frame(self, style="BW.TFrame")
-        self.frame2.pack(fill=X)
+        self.frame2 = Frame(self)
+        self.frame2.pack(fill=X, padx=10, pady=5)
 
-        self.frame3 = ttk.Frame(self, style="BW.TFrame")
-        self.frame3.pack(fill=X)
+        self.frame3 = Frame(self)
+        self.frame3.pack(fill=X, padx=10, pady=5)
 
-        self.frame4 = ttk.Frame(self, style="BW.TFrame")
-        self.frame4.pack(fill=X)
+        self.frame4 = Frame(self)
+        self.frame4.pack(fill=X, padx=10, pady=5)
 
-        self.night_mode_frame = ttk.Frame(self, style="BW.TFrame")
-        self.night_mode_frame.pack(fill=X)
+        self.night_mode_frame = Frame(self)
+        self.night_mode_frame.pack(fill=X, pady=10)
 
     def initLabels(self):
-        self.vram_label = ttk.Label(self.frame1, text="VRAM: ", justify=RIGHT)
-        self.vram_label.pack(side=RIGHT, padx=5, pady=5)
+        self.vram_label = Label(self.frame1, justify=RIGHT, text="VRAM:")
+        self.vram_label.pack(side=RIGHT, padx=5)
 
-        self.gpu_util_label = ttk.Label(self.frame2, text="GPU Util: ", justify=RIGHT)
-        self.gpu_util_label.pack(side=RIGHT, padx=5, pady=5)
+        self.gpu_util_label = Label(self.frame2, justify=RIGHT, text="GPU Util:")
+        self.gpu_util_label.pack(side=RIGHT, padx=5)
 
-        self.ram_label = ttk.Label(self.frame3, text="RAM: ", justify=RIGHT)
-        self.ram_label.pack(side=RIGHT, padx=5, pady=5)
+        self.ram_label = Label(self.frame3, justify=RIGHT, text="RAM:")
+        self.ram_label.pack(side=RIGHT, padx=5)
 
-        self.cpu_util_label = ttk.Label(self.frame4, text="CPU Util: ", justify=RIGHT)
-        self.cpu_util_label.pack(side=RIGHT, padx=5, pady=5)
+        self.cpu_util_label = Label(self.frame4, justify=RIGHT, text="CPU Util:")
+        self.cpu_util_label.pack(side=RIGHT, padx=5)
 
     def initButtons(self):
-        self.night_mode_btn = Button(self.night_mode_frame, text="Night Mode", command=self.toggle_night_mode)
-        self.night_mode_btn.pack(side=RIGHT, padx=5, pady=5)
+        self.night_mode_btn = Button(self.night_mode_frame, command=self.toggle_night_mode)
+        self.night_mode_btn.pack(side=RIGHT, padx=5)
 
     def toggle_night_mode(self):
-        if self.night_mode:
-            self.set_theme("white", "black", "Night Mode")
-        else:
-            self.set_theme("black", "white", "Day Mode")
+        theme = "night" if not self.night_mode else "day"
+        self.set_theme(theme)
         self.night_mode = not self.night_mode
 
-    def set_theme(self, bg_color, fg_color, btn_text):
-        self.style.configure("BW.TFrame", background=bg_color)
-        self.vram_label.config(background=bg_color, foreground=fg_color)
-        self.ram_label.config(background=bg_color, foreground=fg_color)
-        self.cpu_util_label.config(background=bg_color, foreground=fg_color)
-        self.gpu_util_label.config(background=bg_color, foreground=fg_color)
-        self.night_mode_btn.config(text=btn_text)
+    def set_theme(self, theme_name):
+        theme = self.themes[theme_name]
+        bg_color = theme["bg"]
+        fg_color = theme["fg"]
+        btn_text = theme["btn_text"]
+
+        # Configure frames and labels
+        self.master.config(bg=bg_color)
+        for frame in [self, self.frame1, self.frame2, self.frame3, self.frame4, self.night_mode_frame]:
+            frame.config(bg=bg_color)
+
+        # Configure label texts
+        for label in [self.vram_label, self.gpu_util_label, self.ram_label, self.cpu_util_label]:
+            label.config(bg=bg_color, fg=fg_color)
+
+        # Configure the button
+        self.night_mode_btn.config(text=btn_text, bg=bg_color, fg=fg_color)
 
     def update_info(self):
         try:
@@ -81,22 +101,25 @@ class SysInfo(ttk.Frame):
             self.vram_label.config(text="VRAM: GPU not found")
             self.gpu_util_label.config(text="GPU Util: GPU not found")
         except Exception as e:
-            self.vram_label.config(text=f"VRAM: error {str(e)}")
-            self.gpu_util_label.config(text=f"GPU Util: error {str(e)}")
+            self.vram_label.config(text=f"Error: {str(e)}")
+            self.gpu_util_label.config(text=f"Error: {str(e)}")
 
         ram = psutil.virtual_memory()
         ram_perc = round(ram.used / ram.total * 100, 2)
         self.ram_label.config(text=f"RAM: {ram_perc}%")
 
-        cpu_util = round(psutil.cpu_percent(interval=1), 2)
+        cpu_util = round(psutil.cpu_percent(interval=0), 2)
         self.cpu_util_label.config(text=f"CPU Util: {cpu_util}%")
 
         self.after(REFRESH_RATE, self.update_info)
 
 def main():
     root = Tk()
-    root.geometry("300x150+1000+10")
     app = SysInfo()
+    root.update()  # Update to get accurate winfo_width and winfo_height
+    x = (root.winfo_screenwidth() - root.winfo_width()) // 2
+    y = (root.winfo_screenheight() - root.winfo_height()) // 2
+    root.geometry(f"+{x}+{y}")
     root.mainloop()
 
 if __name__ == '__main__':

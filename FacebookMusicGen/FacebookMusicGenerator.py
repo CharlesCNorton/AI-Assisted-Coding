95f786a6214d81259b8267505c1942767680606d
PortfolioAI <135471798+PortfolioAI@users.noreply.github.com>
2023-09-21 16:52:56 -0400

    fixed music generation
    Fixed music generation re: melody function addition.

--- FacebookMusicGen/FacebookMusicGenerator.py
+++ FacebookMusicGen/FacebookMusicGenerator.py
@@ -1,7 +1,9 @@
 import re
 import scipy.io.wavfile
-import traceback
 import torchaudio
+import traceback
+import tkinter as tk
+from tkinter import filedialog
 from colorama import init, Fore
 from datetime import datetime
 from pathlib import Path
@@ -9,8 +11,6 @@ from transformers import AutoProcessor, MusicgenForConditionalGeneration
 from audiocraft.models import MusicGen
 from audiocraft.data.audio import audio_write
 import torch
-import tkinter as tk
-from tkinter import filedialog
 
 init(autoreset=True)
 
@@ -67,33 +67,49 @@ def toggle_device():
     colored_print(Fore.GREEN, f"Switched to {device.type.upper()}.")
 
 def generate_music_with_melody():
-    """Handles the music generation process using an existing melody."""
+    """Generate music based on melody and descriptions."""
+    model = MusicGen.get_pretrained('facebook/musicgen-melody')
+    model.set_generation_params(duration=8)
+
+    root = tk.Tk()
+    root.withdraw()
+    melody_file = filedialog.askopenfilename(title="Select melody file")
+    melody, sr = torchaudio.load(melody_file)
+
+    descriptions = []
+    while True:
+        description = input("Enter description (or hit Enter to skip and finish): ")
+        if not description:
+            break
+        descriptions.append(description)
+
+    if not descriptions:
+        descriptions = [""]
+
+    wav = model.generate_with_chroma(descriptions, melody[None].expand(len(descriptions), -1, -1), sr)
+
+    for idx, one_wav in enumerate(wav):
+        output_file_name = OUTPUT_PATH / f"melody_based_{idx}.wav"
+        audio_write(str(output_file_name), one_wav.cpu(), model.sample_rate, strategy="loudness")
+        colored_print(Fore.GREEN, f"Generated melody-based music saved as {output_file_name.name}")
+
+def generate_music(model_name: str):
+    """Handles the entire music generation process."""
     try:
-        root = tk.Tk()
-        root.withdraw()
-        melody_path = filedialog.askopenfilename(title="Select a melody file", filetypes=[("Audio Files", "*.mp3 *.wav")])
-        if not melody_path:
-            colored_print(Fore.RED, "No melody file selected. Returning to main menu.")
-            return
+        model = load_model(model_name)
+        batch_size = int(input("Enter batch size: ").strip())
+        texts = [input(f"Enter text {i + 1} of {batch_size} (Can be empty): ").strip() for i in range(batch_size)]
 
-        descriptions = []
-        while True:
-            desc = input("Enter description (or hit Enter to skip and finish): ").strip()
-            if not desc:
-                break
-            descriptions.append(desc)
+        for text in texts:
+            audio_array = generate_audio(model, text)
 
-        model = MusicGen.get_pretrained('facebook/musicgen-melody')
-        model.set_generation_params(duration=8)
-        melody, sr = torchaudio.load(melody_path)
+            if not OUTPUT_PATH.exists():
+                OUTPUT_PATH.mkdir(parents=True)
 
-        wav = model.generate_with_chroma(descriptions, melody[None].expand(len(descriptions), -1, -1), sr)
-        
-        for idx, one_wav in enumerate(wav):
-            output_file_name = f"melody_based_{idx}.wav"
-            output_path = OUTPUT_PATH / output_file_name
-            audio_write(str(output_path), one_wav.cpu(), model.sample_rate, strategy="loudness")
-            colored_print(Fore.GREEN, f"Generated melody-based music saved as {output_file_name}")
+            sampling_rate = model.config.audio_encoder.sampling_rate
+            output_file = validate_filename(text)
+            scipy.io.wavfile.write(OUTPUT_PATH / output_file, rate=sampling_rate, data=audio_array)
+            colored_print(Fore.GREEN, f"Music for prompt '{text}' has been successfully saved to {output_file}")
 
     except (OSError, ValueError) as e:
         colored_print(Fore.RED, f"An error occurred: {e}")
@@ -112,19 +128,18 @@ def main():
         colored_print(Fore.MAGENTA, "# m: Generate Music with Melody")
         colored_print(Fore.CYAN, "# t: Toggle CPU/GPU")
         colored_print(Fore.RED, "# q: Quit")
+        choice = input("Enter your choice: ").strip().lower()
 
-        choice = input("Enter your choice: ").strip()
-        if choice in MODEL_MAP:
-            generate_music(MODEL_MAP[choice])
-        elif choice.lower() == 'q':
-            colored_print(Fore.GREEN, "Thank you for using MusicGen. Goodbye!")
+        if choice == "q":
             break
-        elif choice.lower() == 't':
+        elif choice == "t":
             toggle_device()
-        elif choice.lower() == 'm':
+        elif choice == "m":
             generate_music_with_melody()
+        elif choice in MODEL_MAP:
+            generate_music(MODEL_MAP[choice])
         else:
-            colored_print(Fore.RED, "Invalid input. Please enter a valid option.")
+            colored_print(Fore.RED, "Invalid choice! Please try again.")
 
 if __name__ == "__main__":
     main()

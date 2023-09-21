92d789a3a35f615bf17245edbdd8cfd6945d175b
PortfolioAI <135471798+PortfolioAI@users.noreply.github.com>
2023-09-21 11:19:50 -0400

    Persistent Model Loading in VRAM
    - Implemented a caching mechanism to keep models in VRAM after they are loaded.
    - Models now remain in memory for faster subsequent music generations.
    - Enhanced efficiency by reducing redundant model loads from HuggingFace hub or disk.

--- FacebookMusicGen/FacebookMusicGenerator.py
+++ FacebookMusicGen/FacebookMusicGenerator.py
@@ -5,6 +5,7 @@ from colorama import init, Fore
 from datetime import datetime
 from pathlib import Path
 from transformers import AutoProcessor, MusicgenForConditionalGeneration
+import torch
 
 # Initialize colorama
 init(autoreset=True)
@@ -16,6 +17,12 @@ MODEL_MAP = {
     "3": "facebook/musicgen-large",
 }
 OUTPUT_PATH = Path("ENTER_YOUR_DESIRED_PATH")
+
+# Default device based on system configuration
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+# Dictionary to store already loaded models.
+loaded_models = {}
 
 def colored_print(color: str, msg: str):
     """Prints colored messages."""
@@ -34,13 +41,30 @@ def validate_filename(filename: str) -> str:
 
 def load_model(model_name: str):
     """Loads a pretrained model from HuggingFace hub."""
-    return MusicgenForConditionalGeneration.from_pretrained(model_name)
+    global loaded_models
+    # Check if the model is already loaded
+    if model_name not in loaded_models:
+        model = MusicgenForConditionalGeneration.from_pretrained(model_name)
+        loaded_models[model_name] = model.to(device)
+    return loaded_models[model_name]
 
 def generate_audio(model, text: str):
     """Generates audio array from a given text using the provided model."""
     processor = AutoProcessor.from_pretrained(model.config.name_or_path)
     inputs = processor(text=[text], padding=True, return_tensors="pt")
-    return model.generate(**inputs, max_new_tokens=256)[0, 0].numpy()
+    inputs = {key: value.to(device) for key, value in inputs.items()}
+    return model.generate(**inputs, max_new_tokens=1408)[0, 0].cpu().numpy()
+
+def toggle_device():
+    """Toggle between CPU and CUDA GPU."""
+    global device
+    if device.type == "cuda":
+        device = torch.device("cpu")
+    elif torch.cuda.is_available():
+        device = torch.device("cuda")
+    else:
+        colored_print(Fore.RED, "CUDA not available on this system. Staying on CPU.")
+    colored_print(Fore.GREEN, f"Switched to {device.type.upper()}.")
 
 def generate_music(model_name: str):
     """Handles the entire music generation process."""
@@ -53,11 +77,11 @@ def generate_music(model_name: str):
 
         audio_array = generate_audio(model, text)
         filename = validate_filename(input("Enter a name for the output file: ").strip())
-        sampling_rate = model.config.audio_encoder.sampling_rate
 
         if not OUTPUT_PATH.exists():
             OUTPUT_PATH.mkdir(parents=True)
 
+        sampling_rate = model.config.audio_encoder.sampling_rate
         scipy.io.wavfile.write(OUTPUT_PATH / filename, rate=sampling_rate, data=audio_array)
         colored_print(Fore.GREEN, f"Music has been successfully saved to {filename}")
 
@@ -75,6 +99,7 @@ def main():
         colored_print(Fore.GREEN, "# Please select a model:")
         for k, v in MODEL_MAP.items():
             colored_print(Fore.YELLOW, f"# {k}: {v.split('/')[-1]}")
+        colored_print(Fore.CYAN, "# t: Toggle CPU/GPU")
         colored_print(Fore.RED, "# q: Quit")
 
         choice = input("Enter your choice: ").strip()
@@ -83,6 +108,8 @@ def main():
         elif choice.lower() == 'q':
             colored_print(Fore.GREEN, "Thank you for using MusicGen. Goodbye!")
             break
+        elif choice.lower() == 't':
+            toggle_device()
         else:
             colored_print(Fore.RED, "Invalid input. Please enter a valid option.")
 

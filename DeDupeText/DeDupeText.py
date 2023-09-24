d491f32d6f787d380d395e24362cf6ce99ee4406
PortfolioAI <135471798+PortfolioAI@users.noreply.github.com>
2023-09-24 15:32:44 -0400

    Enhance File De-duplication Program for Better Usability and Reliability
    - Introduce logging mechanism for error and event tracking.
    - Add a checksum function for verifying the de-duplication process.
    - Report the total number of lines and duplicates to the user.
    - Prompt the user to confirm before proceeding with de-duplication.
    - Include structured error handling for various exceptions.
    - Refactor code for better modularity and readability.

--- DeDupeText/DeDupeText.py
+++ DeDupeText/DeDupeText.py
@@ -1,28 +1,76 @@
 import tkinter as tk
 from tkinter import filedialog
+import hashlib
+import logging
+import os
+
+# Initialize logging
+logging.basicConfig(level=logging.INFO)
+
+def calculate_checksum(filename):
+    sha256_hash = hashlib.sha256()
+    with open(filename, "rb") as f:
+        for byte_block in iter(lambda: f.read(4096), b""):
+            sha256_hash.update(byte_block)
+    return sha256_hash.hexdigest()
+
+def read_file(filename):
+    with open(filename, 'r') as f:
+        return f.readlines()
+
+def write_file(filename, lines):
+    with open(filename, 'w') as f:
+        for line in lines:
+            f.write(line)
 
 def remove_duplicates_from_file(filename):
-    unique_lines = []
-    seen_lines = set()
+    logging.info(f"Processing {filename} to remove duplicates.")
 
+    # Initial checksum
+    initial_checksum = calculate_checksum(filename)
+
+    # Read and detect duplicates
     try:
-        with open(filename, 'r') as f:
-            for line in f:
-                if line not in seen_lines:
-                    unique_lines.append(line)
-                    seen_lines.add(line)
-
-        with open(filename, 'w') as f:
-            for line in unique_lines:
-                f.write(line)
-
-        print(f"Successfully removed duplicates from {filename}.")
+        lines = read_file(filename)
+        total_lines = len(lines)
+        unique_lines = list(set(lines))
+        duplicates = total_lines - len(unique_lines)
+        
+        print(f"Total lines: {total_lines}")
+        print(f"Duplicate lines: {duplicates}")
+        proceed = input("Do you want to remove duplicates? (y/n): ")
+        if proceed.lower() != 'y':
+            print("Operation canceled.")
+            return
+        write_file(filename, unique_lines)
+    except FileNotFoundError:
+        logging.error(f"The file {filename} was not found.")
+        print(f"The file {filename} was not found.")
+        return
+    except PermissionError:
+        logging.error(f"Permission denied while trying to access {filename}.")
+        print(f"Permission denied while trying to access {filename}.")
+        return
     except Exception as e:
-        print(f"An error occurred: {e}")
+        logging.error(f"An unknown error occurred: {e}")
+        print(f"An unknown error occurred: {e}")
+        return
+    else:
+        # Final checksum
+        final_checksum = calculate_checksum(filename)
+
+        # Report
+        if initial_checksum != final_checksum:
+            logging.info(f"Successfully removed duplicates from {filename}.")
+            print(f"Successfully removed duplicates from {filename}.")
+        else:
+            logging.info(f"No duplicates found in {filename}.")
+            print(f"No duplicates found in {filename}.")
 
 def main():
     root = tk.Tk()
     root.withdraw()
+
     while True:
         print("1. Remove duplicates from a file")
         print("2. Exit")
@@ -30,14 +78,17 @@ def main():
 
         if choice == '1':
             filename = filedialog.askopenfilename(title="Select a text file", filetypes=[("Text files", "*.txt")])
-            if filename:
+            if filename and os.path.exists(filename):
                 remove_duplicates_from_file(filename)
             else:
-                print("No file selected. Please try again.")
+                logging.warning("No valid file selected. Please try again.")
+                print("No valid file selected. Please try again.")
         elif choice == '2':
+            logging.info("Exiting the program. Goodbye!")
             print("Exiting the program. Goodbye!")
             break
         else:
+            logging.warning("Invalid choice entered.")
             print("Invalid choice. Please try again.")
 
 if __name__ == "__main__":

5af4ff3064752df9dd18f8f68efe9cfc1e6b4531
PortfolioAI <135471798+PortfolioAI@users.noreply.github.com>
2023-09-28 19:49:42 -0400

    Comprehensive Enhancement of DeDupeText
    - Integrated verification function to ensure the program's correct functioning. This uses a temporary readme with a duplicate line for testing.
    - Improved overall user interface experience:
      - Added detailed dialog messages for every major operation, ensuring user clarity.
      - Utilized messagebox for more engaging user interaction.
      - Enhanced logging for better troubleshooting and user insights.
    - Refactored the file read and write functions for cleaner separation.
    - Sorted unique lines to retain original order after duplicate removal.
    - Removed backup file creation to simplify the flow and directly overwrite the original file after duplicate removal.
    - Various minor code optimizations for improved readability and performance.

--- DeDupeText/DeDupeText.py
+++ DeDupeText/DeDupeText.py
@@ -1,5 +1,5 @@
 import tkinter as tk
-from tkinter import filedialog
+from tkinter import filedialog, messagebox
 import hashlib
 import logging
 import os
@@ -11,69 +11,91 @@ def calculate_checksum(filename):
             sha256_hash.update(byte_block)
     return sha256_hash.hexdigest()
 
-def read_write_file(filename, mode, lines=None):
-    with open(filename, mode) as f:
-        if mode == 'r':
-            return f.readlines()
-        elif mode == 'w':
-            for line in lines:
-                f.write(line)
+def read_file(filename):
+    with open(filename, 'r') as f:
+        return f.readlines()
+
+def write_file(filename, lines):
+    with open(filename, 'w') as f:
+        for line in lines:
+            f.write(line)
 
 def remove_duplicates_from_file(filename):
-    logging.info(f"Processing {filename} to remove duplicates.")
+    logging.info(f"Processing the file: {filename} to identify duplicate lines.")
+    messagebox.showinfo("Processing", f"Processing the file: {filename} to identify duplicate lines.")
+
     initial_checksum = calculate_checksum(filename)
+
     try:
-        lines = read_write_file(filename, 'r')
+        lines = read_file(filename)
+        unique_lines = sorted(set(lines), key=lines.index)
+
         total_lines = len(lines)
-        unique_lines = list(set(lines))
         duplicates = total_lines - len(unique_lines)
 
-        print(f"Total lines: {total_lines}")
-        print(f"Duplicate lines: {duplicates}")
-        proceed = input("Do you want to remove duplicates? (y/n): ")
-        if proceed.lower() != 'y':
-            print("Operation canceled.")
+        msg = (f"File Analysis Complete!\n\n"
+               f"Total lines: {total_lines}\n"
+               f"Duplicate lines found: {duplicates}\n\n"
+               f"Do you want to remove the duplicate lines?")
+        proceed = messagebox.askyesno("Duplicate Analysis", msg)
+
+        if not proceed:
+            logging.info("User opted to cancel the operation.")
+            messagebox.showinfo("Cancelled", "You chose not to remove duplicates. No changes made to the file.")
             return
 
-        read_write_file(filename, 'w', unique_lines)
+        write_file(filename, unique_lines)
+        messagebox.showinfo("Success", "Duplicates have been successfully removed from the file!")
 
-    except (FileNotFoundError, PermissionError, Exception) as e:
+    except (FileNotFoundError, PermissionError) as e:
         logging.error(f"{type(e).__name__} occurred: {e}")
-        print(f"{type(e).__name__} occurred: {e}")
+        messagebox.showerror("Error", f"{type(e).__name__} occurred: {e}")
+        return
+    except Exception as e:
+        logging.error(f"Unexpected error: {e}")
+        messagebox.showerror("Error", f"Unexpected error: {e}")
         return
 
     final_checksum = calculate_checksum(filename)
-    if initial_checksum != final_checksum:
-        logging.info(f"Successfully removed duplicates from {filename}.")
-        print(f"Successfully removed duplicates from {filename}.")
+    if initial_checksum == final_checksum:
+        messagebox.showinfo("Info", "After analysis, no duplicates were found in the selected file.")
+
+def verify_program():
+    # Create a temporary test file with duplicate lines
+    temp_filename = "temp_readme.txt"
+    with open(temp_filename, 'w') as f:
+        f.write("This is a test line.\n")
+        f.write("This is a duplicate test line.\n")
+        f.write("This is a test line.\n")  # This is a duplicate line
+        f.write("This is another test line.\n")
+
+    remove_duplicates_from_file(temp_filename)
+
+    # Check if the duplicate line has been removed
+    with open(temp_filename, 'r') as f:
+        lines = f.readlines()
+
+    os.remove(temp_filename)  # Clean up the temporary file
+
+    if len(lines) == 3 and lines.count("This is a test line.\n") == 1:
+        messagebox.showinfo("Verification", "Verification Complete: The program is functioning correctly!")
     else:
-        logging.info(f"No duplicates found in {filename}.")
-        print(f"No duplicates found in {filename}.")
+        messagebox.showerror("Verification", "Verification Failed: There seems to be an issue with the program's functionality.")
 
 def main():
     logging.basicConfig(level=logging.INFO)
     root = tk.Tk()
     root.withdraw()
 
-    menu_actions = {'1': lambda: remove_duplicates_from_file(
-                                filedialog.askopenfilename(title="Select a text file",
-                                                           filetypes=[("Text files", "*.txt")])
-                               ),
-                    '2': lambda: print("Exiting the program. Goodbye!")}
+    verify_program()  # Verify that the program works correctly
 
     while True:
-        print("1. Remove duplicates from a file")
-        print("2. Exit")
-        choice = input("Enter your choice: ")
+        filename = filedialog.askopenfilename(title="Select a text file to remove duplicates", filetypes=[("Text files", "*.txt")])
+        if not filename:
+            messagebox.showinfo("Exit", "Thank you for using the Duplicate Remover tool. Goodbye!")
+            break
 
-        action = menu_actions.get(choice)
-        if action:
-            action()
-            if choice == '2':
-                break
-        else:
-            logging.warning("Invalid choice entered.")
-            print("Invalid choice. Please try again.")
+        remove_duplicates_from_file(filename)
 
 if __name__ == "__main__":
     main()

3bf6c7e6a1dc30c57802ae8bc1651894124ce8b8
PortfolioAI <135471798+PortfolioAI@users.noreply.github.com>
2023-10-05 23:57:23 -0400

    Initial commit: Added SafeTensors Converter for .bin to .safetensors conversion with GUI support.

---
+++ SafeTensors Converter/SafetensorsConverter.py
@@ -0,0 +1,95 @@
+import os
+import re
+import torch
+import tkinter as tk
+from tkinter import filedialog, messagebox, ttk
+from safetensors.torch import load_file, save_file
+
+selected_directory = ""
+
+def check_file_size(sf_filename: str, pt_filename: str):
+    sf_size = os.stat(sf_filename).st_size
+    pt_size = os.stat(pt_filename).st_size
+    if (sf_size - pt_size) / pt_size > 0.01:
+        raise RuntimeError(
+            f"""The file size difference is more than 1%:
+         - {sf_filename}: {sf_size}
+         - {pt_filename}: {pt_size}
+         """
+        )
+
+def convert_file(pt_filename: str, sf_filename: str):
+    if os.path.exists(sf_filename):
+        if not messagebox.askyesno("File Exists", f"The file {sf_filename} already exists. Do you want to overwrite it?"):
+            return
+    loaded = torch.load(pt_filename, map_location="cpu")
+    if "state_dict" in loaded:
+        loaded = loaded["state_dict"]
+    loaded = {k: v.contiguous() for k, v in loaded.items()}
+    os.makedirs(os.path.dirname(sf_filename), exist_ok=True)
+    save_file(loaded, sf_filename, metadata={"format": "pt"})
+    check_file_size(sf_filename, pt_filename)
+    reloaded = load_file(sf_filename)
+    for k in loaded:
+        pt_tensor = loaded[k]
+        sf_tensor = reloaded[k]
+        if not torch.equal(pt_tensor, sf_tensor):
+            raise RuntimeError(f"The output tensors do not match for key {k}")
+
+def convert_all_files_in_directory(directory: str):
+    for filename in os.listdir(directory):
+        pt_filename = os.path.join(directory, filename)
+        sf_filename = None
+
+        match = re.match(r"pytorch_model-(\d+)-of-(\d+).bin", filename)
+        if match:
+            part_num, total_parts = match.groups()
+            sf_filename = os.path.join(directory, f"model-{part_num.zfill(5)}-of-{total_parts.zfill(5)}.safetensors")
+
+        elif filename == "pytorch_model.bin":
+            sf_filename = os.path.join(directory, "model.safetensors")
+
+        if sf_filename:
+            convert_file(pt_filename, sf_filename)
+
+def select_directory():
+    global selected_directory
+    selected_directory = filedialog.askdirectory()
+    if selected_directory:
+        directory_label.config(text=f"Selected Directory: {selected_directory}")
+
+def start_conversion():
+    if selected_directory:
+        try:
+            convert_all_files_in_directory(selected_directory)
+            messagebox.showinfo("Success", "Files converted successfully!")
+        except Exception as e:
+            messagebox.showerror("Error", str(e))
+    else:
+        messagebox.showwarning("No Directory Selected", "Please select a directory first.")
+
+def main():
+    global directory_label
+
+    root = tk.Tk()
+    root.title("Bin to SafeTensors Converter")
+
+    frame = ttk.Frame(root, padding="30")
+    frame.grid(row=0, column=0, sticky=(tk.W, tk.E, tk.N, tk.S))
+
+    title_label = ttk.Label(frame, text="Bin to SafeTensors Converter", font=("Arial", 16))
+    title_label.grid(row=0, column=0, pady=20, sticky=tk.W)
+
+    directory_label = ttk.Label(frame, text="Selected Directory: None", font=("Arial", 12))
+    directory_label.grid(row=1, column=0, pady=10, sticky=tk.W)
+
+    select_button = ttk.Button(frame, text="Select Directory", command=select_directory)
+    select_button.grid(row=2, column=0, pady=10)
+
+    convert_button = ttk.Button(frame, text="Convert", command=start_conversion)
+    convert_button.grid(row=3, column=0, pady=20)
+
+    root.mainloop()
+
+if __name__ == "__main__":
+    main()
---
+++ SafeTensors Converter/readme.txt
@@ -0,0 +1,28 @@
+SafeTensors Converter
+=====================
+
+Description:
+------------
+SafeTensors Converter is a graphical tool designed to facilitate the conversion of .bin files to .safetensors format. It provides an intuitive interface to select directories and perform the conversion seamlessly.
+
+Installation:
+-------------
+1. Ensure you have Python 3.x installed.
+2. Install the required packages:
+   - `pip install transformers safetensors torch tkinter`
+3. Clone the repository or download the script.
+
+Usage:
+------
+1. Run the script: `python safe_tensors_converter.py`
+2. Use the "Select Directory" button to choose the directory containing the .bin files.
+3. Click "Convert" to start the conversion process. The converted files will be saved in the same directory with the .safetensors extension.
+
+Note:
+-----
+- Ensure that the directory you choose contains valid .bin files for the conversion.
+- Make sure to have enough storage space in the chosen directory for the converted files.
+
+Support:
+--------
+For issues, suggestions, or queries, please open an issue on the GitHub repository.

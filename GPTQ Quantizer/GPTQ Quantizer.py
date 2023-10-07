d8882197adfef42eff5dfa4f09ae50de23ff29a4
PortfolioAI <135471798+PortfolioAI@users.noreply.github.com>
2023-10-07 00:37:05 -0400

    Refactor GPTQ Quantization Tool
    - Enhanced UI with a cleaner and structured menu.
    - Improved feedback: Added more detailed print statements for user clarity during processes.
    - Introduced `check_model_saved` to verify if model was saved post-quantization.
    - Enhanced error handling: Display detailed tracebacks on exceptions.
    - Streamlined bit selection for quantization.

--- GPTQ Quantizer/GPTQ Quantizer.py
+++ GPTQ Quantizer/GPTQ Quantizer.py
@@ -1,7 +1,9 @@
+import os
 import tkinter as tk
-from tkinter import filedialog, simpledialog
+from tkinter import filedialog
 from transformers import AutoModelForCausalLM, AutoTokenizer, GPTQConfig
 import torch
+import traceback
 
 def select_path(prompt="Select Directory"):
     root = tk.Tk()
@@ -9,55 +11,87 @@ def select_path(prompt="Select Directory"):
     path = filedialog.askdirectory(title=prompt)  # Show the directory dialog
     return path
 
-def get_bit_choice():
-    root = tk.Tk()
-    root.withdraw()  # Hide the root window
-    while True:
-        choice = simpledialog.askstring("Input", "Choose the bit size for quantization (2, 4, 8):")
-        if choice in ['2', '4', '8']:
-            return int(choice)
-        else:
-            print("Invalid bit size. Please choose 2, 4, or 8.")
+def check_model_saved(directory):
+    return any([file.endswith(".bin") for file in os.listdir(directory)])
+
+def print_divider():
+    print("-" * 50)
 
 def main():
-    bit_choice = get_bit_choice()
-
     while True:
-        print("\nQuantization Menu:")
+        print_divider()
+        print("\nGPTQ Quantization Menu:".center(50))
+        print_divider()
         print("1. GPTQ Quantization")
-        print("2. Exit")
+        print("2. Set Quantization Bits (Current: 4)")
+        print("3. Exit")
+        print_divider()
+
         choice = input("\nEnter your choice: ")
 
-        if choice == '1':
-            print(f"Selected {bit_choice}-bit quantization.")
-            print("Loading model...")
-            model_path = select_path("Select Model to Quantize")
-            tokenizer = AutoTokenizer.from_pretrained(model_path)
-            gptq_config = GPTQConfig(bits=bit_choice, dataset="c4", tokenizer=tokenizer)
+        try:
+            if choice == '1':
+                print("\n[INFO] Loading model...")
+                model_path = select_path("Select Model to Quantize")
 
-            # Determine save path before quantizing
-            save_path = select_path("Select Directory to Save Quantized Model")
+                print("[INFO] Loading tokenizer...")
+                tokenizer = AutoTokenizer.from_pretrained(model_path)
 
-            print("Starting GPTQ quantization...")
-            model = AutoModelForCausalLM.from_pretrained(model_path, quantization_config=gptq_config).to("cuda")
+                bits = 4
+                gptq_config = GPTQConfig(bits=bits, dataset="c4", tokenizer=tokenizer)
 
-            # This ensures any backend requirements are met
-            model.disable_exllama = True
+                save_path = select_path("Select Directory to Save Quantized Model")
 
-            # Saving the quantized model
-            print(f"Saving quantized model to {save_path}")
-            model.save_pretrained(save_path)
-            print("GPTQ quantization complete!")
+                print("[INFO] Starting GPTQ quantization...")
 
-            # Cleaning up
-            del model
-            torch.cuda.empty_cache()
+                model = AutoModelForCausalLM.from_pretrained(model_path, quantization_config=gptq_config, device_map='cuda')
 
-        elif choice == '2':
-            break
+                model.disable_exllama = True
 
-        else:
-            print("Invalid choice. Please choose a valid option.")
+                print(f"[INFO] Saving quantized model to {save_path}...")
+                model.save_pretrained(save_path)
+
+                # Check if model was actually saved
+                if check_model_saved(save_path):
+                    print("[SUCCESS] Model saved successfully!")
+                else:
+                    print("[ERROR] Model not found in the specified directory after quantization!")
+
+                # Cleanup
+                print("[INFO] Cleaning up resources...")
+                del model
+                torch.cuda.empty_cache()
+                print("[INFO] Cleanup complete.")
+
+            elif choice == '2':
+                print("\nSet Quantization Bits:")
+                print("a. 2-bit")
+                print("b. 4-bit (Default)")
+                print("c. 8-bit")
+                bit_choice = input("\nSelect bit option (a/b/c): ")
+
+                if bit_choice == 'a':
+                    bits = 2
+                elif bit_choice == 'b':
+                    bits = 4
+                elif bit_choice == 'c':
+                    bits = 8
+                else:
+                    print("[ERROR] Invalid bit selection.")
+                print(f"[INFO] Quantization bits set to {bits}-bit.")
+
+            elif choice == '3':
+                print("[INFO] Exiting program.")
+                break
+
+            else:
+                print("[ERROR] Invalid choice. Please choose a valid option.")
+
+        except Exception as e:
+            print("[ERROR] An unexpected error occurred during the process.")
+            print(f"[DETAILS] {e}")
+            print("[TRACEBACK] Below is the detailed traceback:")
+            traceback.print_exc()
 
 if __name__ == "__main__":
     main()

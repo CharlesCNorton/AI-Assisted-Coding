ae5aaed0ae2c830ab140fb20fe518ee3fc92d421
PortfolioAI <135471798+PortfolioAI@users.noreply.github.com>
2023-10-13 00:10:22 -0400

    Refactor and optimize TextSpecter codebase
    - Organized imports for better readability.
    - Simplified character categorization in `categorize_characters`.
    - Used local variables for frequently accessed data.
    - Renamed 'Odd characters' to 'Infrequent characters' for clarity.
    - Enhanced string manipulations and formatting.
    - Improved output format for a clearer user experience.

--- TextSpecter/TextSpecter.py
+++ TextSpecter/TextSpecter.py
@@ -20,22 +20,21 @@ def read_file(filename):
 def categorize_characters(text):
     char_freq = Counter(text)
 
-    common_chars = set(string.ascii_letters + string.digits + string.punctuation + string.whitespace)
+    common_chars = set(string.ascii_letters + string.digits + string.punctuation)
     whitespace_chars = set(string.whitespace)
     diacritics_chars = set("áéíóúýÁÉÍÓÚÝäëïöüÿÄËÏÖÜàèìòùÀÈÌÒÙãñõÃÑÕâêîôûÂÊÎÔÛçÇ")
     currency_chars = set("¢£¤¥€₹₽₺₩$")
 
-    standard_chars = common_chars - whitespace_chars
     uncommon_chars = set(char_freq.keys()) - common_chars - diacritics_chars - currency_chars
     unicode_punctuations = set(char for char in char_freq.keys() if char in string.punctuation and char not in common_chars)
 
     return {
-        "standard": standard_chars,
+        "standard": common_chars,
         "whitespace": whitespace_chars,
         "diacritics": diacritics_chars,
         "currency": currency_chars,
         "uncommon": uncommon_chars,
-        "unicode_punctuations": unicode_punctuations,
+        "unicode punctuations": unicode_punctuations,
         "frequencies": char_freq
     }
 
@@ -45,24 +44,25 @@ def summarize_characters(filename):
         return
 
     categories = categorize_characters(text)
+    freq = categories["frequencies"]
 
-    print(f"Total characters in '{filename}':", len(text))
-    print("Unique characters:", len(categories["frequencies"]))
+    print(f"Total characters in '{filename}': {len(text)}")
+    print(f"Unique characters: {len(freq)}")
 
     for cat, chars in categories.items():
         if cat != "frequencies":
-            count = sum(categories["frequencies"][char] for char in chars if char in categories["frequencies"])
+            count = sum(freq[char] for char in chars if char in freq)
             if count:
-                print(f"\n{cat.replace('_', ' ').title()} characters (total {count}):")
+                print(f"\n{cat.title()} characters (total {count}):")
                 for char in chars:
-                    if char in categories["frequencies"]:
-                        print(f"'{char}' : {categories['frequencies'][char]}")
+                    if char in freq:
+                        print(f"'{char}' : {freq[char]}")
 
     threshold = 0.01 * len(text)
-    odd_chars = {char: count for char, count in categories["frequencies"].items() if count < threshold}
-    if odd_chars:
-        print("\nOdd characters:")
-        for char, count in odd_chars.items():
+    infrequent_chars = {char: count for char, count in freq.items() if count < threshold}
+    if infrequent_chars:
+        print("\nInfrequent characters:")
+        for char, count in infrequent_chars.items():
             print(f"'{char}' : {count}")
 
 if __name__ == "__main__":

ae8d626e04879e91fc805230bde9cd8722015b31
PortfolioAI <135471798+PortfolioAI@users.noreply.github.com>
2023-10-19 22:20:59 -0400

    Enhanced TextSpecter
    - Refactored main logic into a `main()` function for clarity.
    - Moved `common_words` list to a global constant for better maintainability.
    - Ensured program waits for user input before termination.
    - Refined string formatting for improved output structure.
    - Added exception handling for `filedialog.askopenfilename()`.
    - Sorted characters within categories for consistent output.

--- TextSpecter/TextSpecter.py
+++ TextSpecter/TextSpecter.py
@@ -1,13 +1,22 @@
 import string
 import tkinter as tk
-from tkinter import filedialog
-from collections import Counter, defaultdict
+from tkinter import filedialog, messagebox
+from collections import Counter
+
+COMMON_WORDS = set(["and", "to", "the", "of", "in", "is", "on", "for", "with", "as", "it", "at", "by", "an", "be", "or", "we",
+                    "you", "not", "from", "but", "are", "they", "he", "she", "we", "I", "this", "that", "have", "do", "was",
+                    "can", "will", "my", "your", "his", "her", "its", "our", "their", "there", "when", "where", "how", "why",
+                    "which", "who", "whom", "me", "us", "them"])
 
 def select_file():
-    root = tk.Tk()
-    root.withdraw()
-    file_path = filedialog.askopenfilename(title="Select a text file", filetypes=[("Text files", "*.txt"), ("All files", "*.*")])
-    return file_path
+    try:
+        root = tk.Tk()
+        root.withdraw()
+        file_path = filedialog.askopenfilename(title="Select a text file", filetypes=[("Text files", "*.txt"), ("All files", "*.*")])
+        return file_path
+    except Exception as e:
+        messagebox.showerror("Error", f"Error while selecting the file: {e}")
+        return None
 
 def read_file(filename):
     try:
@@ -20,7 +29,6 @@ def read_file(filename):
 def categorize_characters(text):
     char_freq = Counter(text)
 
-    # Character categories
     common_chars = set(string.ascii_letters + string.digits + string.punctuation)
     whitespace_chars = set(string.whitespace)
     diacritics_chars = set("áéíóúýÁÉÍÓÚÝäëïöüÿÄËÏÖÜàèìòùÀÈÌÒÙãñõÃÑÕâêîôûÂÊÎÔÛçÇ")
@@ -39,9 +47,8 @@ def categorize_characters(text):
     }
 
 def count_common_words(text):
-    common_words = ["and","to","the","of","in","is","on","for","with","as","it","at","by","an","be","or","we","you","not","from","but","are","they","he","she","we","I","this","that","have","do","was","can","will","my","your","his","her","its","our","their","there","when","where","how","why","which","who","whom","me","us","them"]
     word_freq = Counter(text.lower().split())
-    return {word: word_freq[word] for word in common_words if word in word_freq}
+    return {word: word_freq[word] for word in COMMON_WORDS if word in word_freq}
 
 def summarize_characters(filename):
     text = read_file(filename)
@@ -51,20 +58,19 @@ def summarize_characters(filename):
     categories = categorize_characters(text)
     freq = categories["frequencies"]
 
-    print(f"Total characters in '{filename}': {len(text)}")
+    print(f"\nSummary for '{filename}':")
+    print(f"\nTotal characters: {len(text)}")
     print(f"Unique characters: {len(freq)}")
 
-    # Summarizing character categories
     for cat, chars in categories.items():
         if cat != "frequencies":
             count = sum(freq[char] for char in chars if char in freq)
             if count:
                 print(f"\n{cat.title()} characters (total {count}):")
-                for char in chars:
+                for char in sorted(chars):
                     if char in freq:
                         print(f"'{char}' : {freq[char]}")
 
-    # Summarizing infrequent characters
     threshold = 0.01 * len(text)
     infrequent_chars = {char: count for char, count in freq.items() if count < threshold}
     if infrequent_chars:
@@ -72,16 +78,19 @@ def summarize_characters(filename):
         for char, count in sorted(infrequent_chars.items(), key=lambda x: x[1]):
             print(f"'{char}' : {count}")
 
-    # Summarizing common words
     common_word_counts = count_common_words(text)
     if common_word_counts:
         print("\nCommon word counts:")
         for word, count in sorted(common_word_counts.items(), key=lambda x: x[1], reverse=True):
             print(f"{word} : {count}")
 
-if __name__ == "__main__":
+def main():
     file_path = select_file()
     if file_path:
         summarize_characters(file_path)
     else:
         print("No file selected.")
+    input("\nPress Enter to exit...")
+
+if __name__ == "__main__":
+    main()

06c85aac45c9c6144aa7c82cb66ed740e9bed6ff
PortfolioAI <135471798+PortfolioAI@users.noreply.github.com>
2023-10-28 23:39:00 -0400

    Implement `time_it` decorator and refactor main function for performance tracking

--- GPTQ Quantizer/GPTQ Quantizer.py
+++ GPTQ Quantizer/GPTQ Quantizer.py
@@ -4,6 +4,7 @@ from tkinter import filedialog
 from transformers import AutoModelForCausalLM, AutoTokenizer, GPTQConfig
 import torch
 import traceback
+import time
 
 def select_path(prompt="Select Directory"):
     root = tk.Tk()
@@ -16,6 +17,31 @@ def check_model_saved(directory):
 
 def print_divider():
     print("-" * 50)
+
+def time_it(func):
+    def wrapper(*args, **kwargs):
+        start_time = time.time()
+        result = func(*args, **kwargs)
+        end_time = time.time()
+        print(f"[TIME] {func.__name__} took {end_time - start_time:.2f} seconds.")
+        return result
+    return wrapper
+
+@time_it
+def load_tokenizer(model_path):
+    return AutoTokenizer.from_pretrained(model_path)
+
+@time_it
+def perform_quantization(model_path, gptq_config):
+    print("[INFO] Loading model into VRAM...")
+    model = AutoModelForCausalLM.from_pretrained(model_path, quantization_config=gptq_config, device_map='cuda')
+    print("[INFO] Model loaded into VRAM.")
+
+    return model
+
+@time_it
+def save_quantized_model(model, save_path):
+    model.save_pretrained(save_path)
 
 def main():
     global bits
@@ -34,31 +60,34 @@ def main():
 
         try:
             if choice == '1':
-                print("\n[INFO] Loading model...")
+                print("\n[INFO] Selecting model path...")
                 model_path = select_path("Select Model to Quantize")
+                if not model_path:
+                    print("[INFO] No model path selected, skipping.")
+                    continue
 
                 print("[INFO] Loading tokenizer...")
-                tokenizer = AutoTokenizer.from_pretrained(model_path)
+                tokenizer = load_tokenizer(model_path)
 
+                print("[INFO] Preparing quantization config...")
                 gptq_config = GPTQConfig(bits=bits, dataset="c4", tokenizer=tokenizer)
 
+                print("[INFO] Selecting save path...")
                 save_path = select_path("Select Directory to Save Quantized Model")
+                if not save_path:
+                    print("[INFO] No save path selected, skipping.")
+                    continue
 
                 print("[INFO] Starting GPTQ quantization...")
-
-                model = AutoModelForCausalLM.from_pretrained(model_path, quantization_config=gptq_config, device_map='cuda')
-
-                model.disable_exllama = True
+                model = perform_quantization(model_path, gptq_config)
 
                 print(f"[INFO] Saving quantized model to {save_path}...")
-                model.save_pretrained(save_path)
-
+                save_quantized_model(model, save_path)
 
                 if check_model_saved(save_path):
                     print("[SUCCESS] Model saved successfully!")
                 else:
                     print("[ERROR] Model not found in the specified directory after quantization!")
-
 
                 print("[INFO] Cleaning up resources...")
                 del model

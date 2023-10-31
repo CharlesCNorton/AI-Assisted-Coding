ba38aa5a20c4aedcd04e3def7fa33f44f2a68e24
PortfolioAI <135471798+PortfolioAI@users.noreply.github.com>
2023-10-31 15:08:38 -0400

    Initial commit: Introducing DataMingle - A Text Scrambler for AI Training datasets.
    This commit introduces the first version of DataMingle, a GUI tool designed to shuffle lines within text datasets. The application is developed with tkinter and aims to aid in the preparation of datasets for training language models by ensuring a more randomized order of data.
    
    Main Features:
    - User-friendly GUI interface.
    - File dialogs for easy file selection and output path determination.
    - Status bar to keep the user informed.
    - Error handling mechanisms for better user experience.

---
+++ DataMingle/DataMingle.py
@@ -0,0 +1,109 @@
+import tkinter as tk
+from tkinter import filedialog, messagebox
+import random
+
+class DataMingle:
+    def __init__(self, master):
+        self.master = master
+        self.master.title("DataMingle: Text Scrambler for AI Training")
+
+        self.input_file_var = tk.StringVar()
+        self.output_file_var = tk.StringVar()
+
+        # Menu
+        self.build_menu()
+
+        # Input file display
+        self.build_input_display()
+
+        # Output file display
+        self.build_output_display()
+
+        # Shuffle button
+        self.build_shuffle_button()
+
+        # Status bar
+        self.build_status_bar()
+
+    def build_menu(self):
+        menu = tk.Menu(self.master)
+        self.master.config(menu=menu)
+
+        file_menu = tk.Menu(menu)
+        menu.add_cascade(label="File", menu=file_menu)
+        file_menu.add_command(label="Select Input File", command=self.select_input_file)
+        file_menu.add_command(label="Select Output Path", command=self.select_output_path)
+        file_menu.add_command(label="Clear Paths", command=self.clear_paths)
+        file_menu.add_separator()
+        file_menu.add_command(label="Exit", command=self.master.quit)
+
+    def build_input_display(self):
+        input_label = tk.Label(self.master, text="Input File:")
+        input_label.pack(pady=20)
+
+        input_display = tk.Entry(self.master, textvariable=self.input_file_var, width=50)
+        input_display.pack(pady=5)
+
+    def build_output_display(self):
+        output_label = tk.Label(self.master, text="Output File:")
+        output_label.pack(pady=20)
+
+        output_display = tk.Entry(self.master, textvariable=self.output_file_var, width=50)
+        output_display.pack(pady=5)
+
+    def build_shuffle_button(self):
+        shuffle_button = tk.Button(self.master, text="Shuffle and Save", command=self.shuffle_and_save)
+        shuffle_button.pack(pady=20)
+
+    def build_status_bar(self):
+        self.status = tk.StringVar()
+        self.status.set("Ready.")
+        status_bar = tk.Label(self.master, textvariable=self.status, bd=1, relief=tk.SUNKEN, anchor=tk.W)
+        status_bar.pack(side=tk.BOTTOM, fill=tk.X)
+
+    def select_input_file(self):
+        file_path = filedialog.askopenfilename(title="Select the input text file", filetypes=[("Text files", "*.txt")])
+        if file_path:
+            self.input_file_var.set(file_path)
+
+    def select_output_path(self):
+        file_path = filedialog.asksaveasfilename(title="Select the output path", filetypes=[("Text files", "*.txt")], defaultextension=".txt")
+        if file_path:
+            self.output_file_var.set(file_path)
+
+    def clear_paths(self):
+        self.input_file_var.set("")
+        self.output_file_var.set("")
+        self.status.set("Paths cleared.")
+
+    def shuffle_and_save(self):
+        input_file = self.input_file_var.get()
+        output_file = self.output_file_var.get()
+
+        if not input_file:
+            messagebox.showerror("Error", "Please select an input file!")
+            return
+
+        if not output_file:
+            messagebox.showerror("Error", "Please select an output file!")
+            return
+
+        self.status.set("Processing...")
+        try:
+            with open(input_file, 'r') as file:
+                lines = file.readlines()
+
+            random.shuffle(lines)
+
+            with open(output_file, 'w') as file:
+                file.writelines(lines)
+
+            self.status.set(f"Done! Shuffled data saved to {output_file}")
+        except Exception as e:
+            self.status.set("Error encountered.")
+            messagebox.showerror("Error", str(e))
+
+if __name__ == "__main__":
+    root = tk.Tk()
+    app = DataMingle(root)
+    root.mainloop()
---
+++ DataMingle/readme.txt
@@ -0,0 +1,24 @@
+# DataMingle: Text Scrambler for AI Training
+
+**DataMingle** is a user-friendly GUI tool designed to help shuffle the lines within text datasets. This can be particularly useful when preparing datasets for training language models, ensuring a well-mixed dataset that enhances the training process.
+
+## Features
+
+- **Easy-to-use GUI**: No need to use command lines. A simple and intuitive interface is provided.
+- **File Pickers**: Easily select input text files and determine output paths using the in-built file dialogs.
+- **Status Updates**: Stay informed with the handy status bar at the bottom of the application.
+- **Error Handling**: Any issues with file selection, reading, or writing are gracefully managed with informative pop-up messages.
+
+## Usage
+
+1. Launch `DataMingle`.
+2. Use the `File` menu to:
+    - Select your input file.
+    - Define your output path.
+3. Click on the `Shuffle and Save` button.
+4. Your shuffled dataset is now saved to the specified output path!
+
+## Requirements
+
+- Python (3.x recommended)
+- tkinter (usually comes pre-installed with Python)

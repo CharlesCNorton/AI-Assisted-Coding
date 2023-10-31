122fd99ccbc12f7f14e874b8f67b61073435b1fa
PortfolioAI <135471798+PortfolioAI@users.noreply.github.com>
2023-10-30 23:52:55 -0400

    Refactor and enhance the LinguaLens tkinter application.
    - Organized code into separate classes and methods for improved readability and maintainability.
    - Enhanced exception handling with more specific error messages.
    - Modularized sentence analysis logic into a dedicated `SentenceAnalyzer` class.
    - Introduced the `LinguaLensApp` class to handle the main application UI and logic.
    - Added descriptive comments for better code understanding.

--- LinguaLens/LinguaLens.py
+++ LinguaLens/LinguaLens.py
@@ -2,67 +2,102 @@ import tkinter as tk
 from tkinter import filedialog, messagebox, Text, Scrollbar, Toplevel
 from collections import defaultdict
 
-def categorize_length(sentence):
-    words = len(sentence.split())
-    if 1 <= words <= 5:
-        return "Short"
-    elif 6 <= words <= 15:
-        return "Medium"
-    elif 16 <= words <= 50:
-        return "Large"
-    else:
-        return "Other"
+class SentenceAnalyzer:
+    @staticmethod
+    def categorize_length(sentence):
+        words = len(sentence.split())
+        if 1 <= words <= 5:
+            return "Short"
+        elif 6 <= words <= 15:
+            return "Medium"
+        elif 16 <= words <= 50:
+            return "Large"
+        else:
+            return "Other"
 
-def categorize_ending(sentence):
-    if sentence.endswith("?"):
-        return "Question"
-    elif sentence.endswith("."):
-        return "Statement"
-    elif sentence.endswith("!"):
-        return "Exclamation"
-    else:
-        return "Other"
+    @staticmethod
+    def categorize_ending(sentence):
+        if sentence.endswith("?"):
+            return "Question"
+        elif sentence.endswith("."):
+            return "Statement"
+        elif sentence.endswith("!"):
+            return "Exclamation"
+        else:
+            return "Other"
 
-def analyze_file(file_path):
-    try:
-        with open(file_path, 'r', encoding="utf-8") as file:
-            lines = file.readlines()
+    @staticmethod
+    def analyze_file(file_path):
+        try:
+            with open(file_path, 'r', encoding="utf-8") as file:
+                lines = file.readlines()
 
-        data = defaultdict(lambda: defaultdict(int))
-        total_sentences = 0
+            data = defaultdict(lambda: defaultdict(int))
+            total_sentences = 0
 
-        for line in lines:
-            line = line.strip()
-            length_cat = categorize_length(line)
-            ending_cat = categorize_ending(line)
-            data[length_cat][ending_cat] += 1
-            total_sentences += 1
+            for line in lines:
+                line = line.strip()
+                length_cat = SentenceAnalyzer.categorize_length(line)
+                ending_cat = SentenceAnalyzer.categorize_ending(line)
+                data[length_cat][ending_cat] += 1
+                total_sentences += 1
 
-        summary = f"Analysis of '{file_path.split('/')[-1]}':\nTotal Sentences: {total_sentences:,.0f}\n" + "-"*40 + "\n"
-        results.delete(1.0, tk.END)
-        results.insert(tk.END, summary)
+            summary = f"Analysis of '{file_path.split('/')[-1]}':\nTotal Sentences: {total_sentences:,.0f}\n" + "-" * 40 + "\n"
+            results.delete(1.0, tk.END)
+            results.insert(tk.END, summary)
 
-        for length in ["Large", "Medium", "Short", "Other"]:
-            if length in data:
-                results.insert(tk.END, f"{length} Sentences:\n")
-                for ending, count in data[length].items():
-                    results.insert(tk.END, f" - {ending}: {count:,.0f}\n")
-                results.insert(tk.END, "\n")
+            for length in ["Large", "Medium", "Short", "Other"]:
+                if length in data:
+                    results.insert(tk.END, f"{length} Sentences:\n")
+                    for ending, count in data[length].items():
+                        results.insert(tk.END, f" - {ending}: {count:,.0f}\n")
+                    results.insert(tk.END, "\n")
 
-    except FileNotFoundError:
-        messagebox.showerror("Error", "File not found!")
-    except Exception as e:
-        messagebox.showerror("Error", f"An error occurred: {e}")
+        except FileNotFoundError:
+            messagebox.showerror("Error", "File not found!")
+        except Exception as e:
+            messagebox.showerror("Error", f"An error occurred: {e}")
 
-def browse_files():
-    filename = filedialog.askopenfilename(filetypes =[("Text files", "*.txt")])
-    if filename:
-        analyze_file(filename)
 
-def show_help():
-    help_window = Toplevel(root)
-    help_window.title("Help")
-    help_text = """LinguaLens - Dataset Examination Tool
+class LinguaLensApp:
+    def __init__(self, root):
+        self.root = root
+        self.setup_ui()
+
+    def setup_ui(self):
+        self.root.title("LinguaLens")
+        self.root.geometry("500x500")
+
+        menu = tk.Menu(self.root)
+        self.root.config(menu=menu)
+
+        file_menu = tk.Menu(menu)
+        menu.add_cascade(label="File", menu=file_menu)
+        file_menu.add_command(label="Open...", command=self.browse_files)
+        file_menu.add_command(label="Exit", command=self.root.quit)
+
+        help_menu = tk.Menu(menu)
+        menu.add_cascade(label="Help", menu=help_menu)
+        help_menu.add_command(label="About", command=self.show_help)
+
+        global results
+        results = Text(self.root, wrap=tk.WORD, height=24, width=60)
+        results.pack(padx=10, pady=10)
+
+        scroll = Scrollbar(self.root, command=results.yview)
+        scroll.pack(side=tk.RIGHT, fill=tk.Y)
+
+        results.configure(yscrollcommand=scroll.set)
+
+    def browse_files(self):
+        filename = filedialog.askopenfilename(filetypes=[("Text files", "*.txt")])
+        if filename:
+            SentenceAnalyzer.analyze_file(filename)
+
+    def show_help(self):
+        help_window = Toplevel(self.root)
+        help_window.title("Help")
+        help_text = """LinguaLens - Dataset Examination Tool
 
 1. Browse to select a text file for analysis.
 2. The application categorizes sentences into:
@@ -74,31 +109,11 @@ def show_help():
 3. It also classifies endings as Questions, Statements, or Exclamations.
 
 This tool helps to analyze the symmetry of datasets intended for language model fine-tuning."""
-    label = tk.Label(help_window, text=help_text, padx=10, pady=10, justify=tk.LEFT)
-    label.pack()
+        label = tk.Label(help_window, text=help_text, padx=10, pady=10, justify=tk.LEFT)
+        label.pack()
 
-root = tk.Tk()
-root.title("LinguaLens")
-root.geometry("500x500")
 
-menu = tk.Menu(root)
-root.config(menu=menu)
-
-file_menu = tk.Menu(menu)
-menu.add_cascade(label="File", menu=file_menu)
-file_menu.add_command(label="Open...", command=browse_files)
-file_menu.add_command(label="Exit", command=root.quit)
-
-help_menu = tk.Menu(menu)
-menu.add_cascade(label="Help", menu=help_menu)
-help_menu.add_command(label="About", command=show_help)
-
-results = Text(root, wrap=tk.WORD, height=24, width=60)
-results.pack(padx=10, pady=10)
-
-scroll = Scrollbar(root, command=results.yview)
-scroll.pack(side=tk.RIGHT, fill=tk.Y)
-
-results.configure(yscrollcommand=scroll.set)
-
-root.mainloop()
+if __name__ == "__main__":
+    root = tk.Tk()
+    app = LinguaLensApp(root)
+    root.mainloop()

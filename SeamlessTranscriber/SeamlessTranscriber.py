80f0c1b5622003dda4cfcafff6d3da220632321b
PortfolioAI <135471798+PortfolioAI@users.noreply.github.com>
2023-12-11 22:15:10 -0500

    Refactor and Enhance
    1. Code Modularization:
       - Introduced new functions `record_audio`, `save_audio_to_file`, and `get_user_input` for specific, focused tasks, improving code readability and maintainability.
       - Each function is now responsible for a single aspect of the program, making the codebase more organized and easier to understand.
    
    2. Robust Error Handling:
       - Added comprehensive try-except blocks in functions dealing with audio recording, file operations, and transcription to catch and manage potential exceptions.
       - Error messages are now more informative, aiding in quicker debugging and user understanding.
    
    3. User Input Validation:
       - Implemented a new function `get_user_input` to handle all user inputs with error checking and the ability to handle unexpected interruptions (like KeyboardInterrupt).

--- SeamlessTranscriber/SeamlessTranscriber.py
+++ SeamlessTranscriber/SeamlessTranscriber.py
@@ -9,31 +9,50 @@ sys.path.append('ENTER_PATH_HERE')
 
 from seamless_communication.models.inference import Translator
 
-def record_from_microphone(duration=10):
-    """Record audio from the microphone and return the NumPy array."""
-    samplerate = 16000
-    audio = sd.rec(int(samplerate * duration), samplerate=samplerate, channels=1, dtype='float32')
-    sd.wait()
-    return audio.squeeze()
+def record_audio(samplerate=16000, duration=10):
+    """Record audio from the microphone."""
+    try:
+        audio = sd.rec(int(samplerate * duration), samplerate=samplerate, channels=1, dtype='float32')
+        sd.wait()
+        return audio.squeeze()
+    except Exception as e:
+        print(f"Error recording audio: {e}")
+        return None
+
+def save_audio_to_file(audio, filename='temp_recording.wav', samplerate=16000):
+    """Save audio to a file."""
+    try:
+        torchaudio.save(filename, torch.tensor(audio).unsqueeze(0), samplerate)
+    except Exception as e:
+        print(f"Error saving audio file: {e}")
 
 def transcribe_audio(model, audio_path=None):
     """Transcribe audio using the ASR model."""
     if not audio_path:
-        audio = record_from_microphone()
+        audio = record_audio()
+        if audio is None:
+            return "Recording failed. Cannot transcribe."
         audio_path = 'temp_recording.wav'
-        torchaudio.save(audio_path, torch.tensor(audio).unsqueeze(0), 16000)
+        save_audio_to_file(audio, audio_path)
+    try:
+        transcribed_text, _, _ = model.predict(audio_path, "asr", "en")
+        return transcribed_text
+    except Exception as e:
+        return f"Error in transcription: {e}"
 
-    transcribed_text, _, _ = model.predict(audio_path, "asr", "en")
-    return transcribed_text
+def get_user_input(prompt):
+    """Get user input with a prompt."""
+    try:
+        return input(prompt)
+    except KeyboardInterrupt:
+        print("\nOperation cancelled by user.")
+        return None
 
 def main():
     print("Initializing the SeamlessM4T model...")
-
-    # Initialize the Translator object
     model_checkpoint = "ENTER_PATH_HERE"
     vocoder_checkpoint = "ENTER_PATH_HERE"
     translator = Translator(model_checkpoint, vocoder_name_or_card=vocoder_checkpoint, device=torch.device("cuda:0" if torch.cuda.is_available() else "cpu"))
-
     print("\nSeamlessM4T model initialized!")
 
     while True:
@@ -42,10 +61,11 @@ def main():
         print("2. Transcribe directly from the microphone.")
         print("3. Exit.")
 
-        choice = input("\nEnter your choice: ")
-
+        choice = get_user_input("\nEnter your choice: ")
         if choice == '1':
-            audio_path = input("Enter the path to the audio file: ")
+            audio_path = get_user_input("Enter the path to the audio file: ")
+            if not audio_path:
+                continue
             if os.path.exists(audio_path):
                 print("\nTranscribing...")
                 text = transcribe_audio(translator, audio_path)

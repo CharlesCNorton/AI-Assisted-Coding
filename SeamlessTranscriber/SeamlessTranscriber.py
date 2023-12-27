95dfc58834b6a4b9a21a7f8b2423787d8504e375
PortfolioAI <135471798+PortfolioAI@users.noreply.github.com>
2023-12-26 23:57:56 -0500

    Enhanced Error Handling and User Input Management in Audio Transcription Program
    - Improved error handling in `record_audio` with specific checks for null audio data.
    - Added verification in `save_audio_to_file` to ensure audio data exists before saving.
    - Implemented file existence check in `transcribe_audio` for validating the audio file path.
    - Incorporated try-except block around model initialization in `main` for better error management.
    - Enabled graceful program exit on `KeyboardInterrupt` for user-friendly operation.
    - Refactored model and vocoder checkpoint paths for easier configuration.
    - Program now exits on critical failures (model initialization failure, user interrupt).
    - General code clean-up for enhanced readability and maintainability.

--- SeamlessTranscriber/SeamlessTranscriber.py
+++ SeamlessTranscriber/SeamlessTranscriber.py
@@ -14,6 +14,8 @@ def record_audio(samplerate=16000, duration=10):
     try:
         audio = sd.rec(int(samplerate * duration), samplerate=samplerate, channels=1, dtype='float32')
         sd.wait()
+        if audio is None:
+            raise ValueError("No audio data recorded.")
         return audio.squeeze()
     except Exception as e:
         print(f"Error recording audio: {e}")
@@ -21,6 +23,9 @@ def record_audio(samplerate=16000, duration=10):
 
 def save_audio_to_file(audio, filename='temp_recording.wav', samplerate=16000):
     """Save audio to a file."""
+    if audio is None:
+        print("No audio data to save.")
+        return
     try:
         torchaudio.save(filename, torch.tensor(audio).unsqueeze(0), samplerate)
     except Exception as e:
@@ -34,6 +39,8 @@ def transcribe_audio(model, audio_path=None):
             return "Recording failed. Cannot transcribe."
         audio_path = 'temp_recording.wav'
         save_audio_to_file(audio, audio_path)
+    if not os.path.exists(audio_path):
+        return "Audio file does not exist. Cannot transcribe."
     try:
         transcribed_text, _, _ = model.predict(audio_path, "asr", "en")
         return transcribed_text
@@ -46,13 +53,17 @@ def get_user_input(prompt):
         return input(prompt)
     except KeyboardInterrupt:
         print("\nOperation cancelled by user.")
-        return None
+        sys.exit(0)
 
 def main():
     print("Initializing the SeamlessM4T model...")
-    model_checkpoint = "ENTER_PATH_HERE"
-    vocoder_checkpoint = "ENTER_PATH_HERE"
-    translator = Translator(model_checkpoint, vocoder_name_or_card=vocoder_checkpoint, device=torch.device("cuda:0" if torch.cuda.is_available() else "cpu"))
+    model_checkpoint = "ENTER_MODEL_CHECKPOINT_PATH"
+    vocoder_checkpoint = "ENTER_VOCODER_CHECKPOINT_PATH"
+    try:
+        translator = Translator(model_checkpoint, vocoder_name_or_card=vocoder_checkpoint, device=torch.device("cuda:0" if torch.cuda.is_available() else "cpu"))
+    except Exception as e:
+        print(f"Failed to initialize the model: {e}")
+        sys.exit(1)
     print("\nSeamlessM4T model initialized!")
 
     while True:
@@ -66,12 +77,9 @@ def main():
             audio_path = get_user_input("Enter the path to the audio file: ")
             if not audio_path:
                 continue
-            if os.path.exists(audio_path):
-                print("\nTranscribing...")
-                text = transcribe_audio(translator, audio_path)
-                print("\nTranscribed Text:\n", text)
-            else:
-                print("\nFile not found. Please check the path and try again.")
+            print("\nTranscribing...")
+            text = transcribe_audio(translator, audio_path)
+            print("\nTranscribed Text:\n", text)
         elif choice == '2':
             print("\nRecording audio from the microphone... (Duration: 10 seconds)")
             text = transcribe_audio(translator)

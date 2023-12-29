d794196fab78fbab631c22e8806f5d4ee3799cf7
PortfolioAI <135471798+PortfolioAI@users.noreply.github.com>
2023-12-29 13:29:02 -0500

    Enhanced InfernoLM with TTS, Interactive Chat, and UI Improvements
    - Introduce text-to-speech functionality with strategy pattern implementation.
    - Implement color-coded console outputs for enhanced user experience.
    - Refine model loading and tokenization with improved error handling.
    - Add interactive chat with assistant feature for user engagement.
    - Include verbose logging toggle for detailed operation insights.
    - Overhaul user interface with a structured menu-driven approach.
    - Improve code organization, resource management, and error debugging.

--- TextGenerator/textgenerator.py
+++ TextGenerator/textgenerator.py
@@ -2,124 +2,161 @@ import os
 import logging
 import warnings
 import torch
-import gc
 import tkinter as tk
 from tkinter import filedialog
-from colorama import init, Fore, Style
+from colorama import init, Fore, Back, Style
 from transformers import AutoTokenizer, AutoModelForCausalLM
 import traceback
-
-os.environ['TF_ENABLE_ONEDNN_OPTS'] = '0'
-
-logging.getLogger('tensorflow').setLevel(logging.ERROR)
-
-warnings.filterwarnings("ignore")
+import pyttsx3
+from abc import ABC, abstractmethod
 
 init(autoreset=True)
 
+class TTSStrategy(ABC):
+    @abstractmethod
+    def speak(self, text):
+        pass
+
+class Pyttsx3Strategy(TTSStrategy):
+    def __init__(self):
+        self.engine = pyttsx3.init()
+
+    def speak(self, text):
+        try:
+            self.engine.say(text)
+            self.engine.runAndWait()
+        except Exception as e:
+            print_colored("Error in Pyttsx3 TTS: " + str(e), color=Fore.RED)
+
+def print_colored(text, color=Fore.WHITE, on_color='', attrs=[], end='\n'):
+    print(f"{color}{on_color}{''.join(attrs)}{text}{Style.RESET_ALL}", end=end)
+
+os.environ['TF_ENABLE_ONEDNN_OPTS'] = '0'
+logging.basicConfig(level=logging.INFO)
+logging.getLogger('tensorflow').setLevel(logging.ERROR)
+warnings.filterwarnings("ignore")
+
 class InfernoLM:
-    def __init__(self, device="cpu", precision="float32", model_path=None):
+    def __init__(self, device="cpu", precision="float32", model_path=None, verbose=False, tts_strategy=None):
         self.device = device
         self.precision = precision
         self.model_path = model_path
-        torch.cuda.empty_cache()
-        gc.collect()
-        self.tokenizer, self.model = self._load_model_and_tokenizer()
+        self.verbose = verbose
+        self.tts_strategy = tts_strategy
+        self.tts_enabled = False if tts_strategy is None else True
+        if model_path:
+            self.tokenizer, self.model = self._load_model_and_tokenizer()
 
     def _load_model_and_tokenizer(self):
-        try:
-            tokenizer = AutoTokenizer.from_pretrained(self.model_path, local_files_only=True)
-            model = AutoModelForCausalLM.from_pretrained(self.model_path, local_files_only=True)
+        print_colored("Loading model and tokenizer...", color=Fore.YELLOW)
+        tokenizer = AutoTokenizer.from_pretrained(self.model_path, local_files_only=True)
+        model = AutoModelForCausalLM.from_pretrained(self.model_path, local_files_only=True)
+        model.config.pad_token_id = model.config.eos_token_id
+        tokenizer.pad_token = tokenizer.eos_token
+        model.to(device=self.device, dtype=torch.float16 if self.precision == "float16" else torch.float32)
+        return tokenizer, model
 
-            model.config.pad_token_id = model.config.eos_token_id
+    def toggle_verbose_logging(self):
+        self.verbose = not self.verbose
+        logging_level = logging.DEBUG if self.verbose else logging.INFO
+        logging.basicConfig(level=logging_level)
+        print_colored(f"Verbose logging {'enabled' if self.verbose else 'disabled'}.", color=Fore.BLUE)
 
-            model.config.do_sample = True
-            model.config.temperature = 1.0
-            model.config.top_p = 0.9
-            model.config.top_k = 50
-            model.save_pretrained(self.model_path)
-            tokenizer.add_special_tokens({'pad_token': '[PAD]'})
-            model.to(device=self.device, dtype=torch.float16 if self.precision == "float16" else torch.float32)
-            return tokenizer, model
-        except Exception as e:
-            print(f"An error occurred while loading the model: {str(e)}")
-            traceback.print_exc()
+    def load_model(self):
+        model_path = select_path()
+        if model_path:
+            self.model_path = model_path
+            self.choose_device()
+            self.choose_precision()
 
-    def infer_text(self, prompt, mode="full", max_length=100, temperature=0.7, top_p=0.9, real_time=False):
-        try:
-            inputs = self.tokenizer(prompt, return_tensors='pt', padding=True, truncation=True, max_length=max_length).to(self.device)
-            generation_config = {
-                'input_ids': inputs['input_ids'],
-                'max_length': max_length,
-                'temperature': temperature,
-                'attention_mask': inputs['attention_mask'],
-                'do_sample': True,
-                'top_p': top_p,
-                'top_k': 50
-            }
+            self.tokenizer, self.model = self._load_model_and_tokenizer()
+            print_colored(f"Model loaded from {model_path}", color=Fore.CYAN)
+        else:
+            print_colored("No directory selected.", color=Fore.RED)
 
-            if real_time:
-                print("Starting real-time token-by-token generation...")
-                output_tokens = []
-                for i in range(max_length):
-                    generation_config['max_length'] = i + 1
-                    outputs = self.model.generate(**generation_config)
-                    output_tokens.append(outputs[0][-1].item())
-                    print(self.tokenizer.decode([output_tokens[-1]], skip_special_tokens=True), end='', flush=True)
-                inferred_text = self.tokenizer.decode(output_tokens, skip_special_tokens=True)
-            else:
-                outputs = self.model.generate(**generation_config)
-                inferred_text = self.tokenizer.decode(outputs[0], skip_special_tokens=True)
+    def choose_device(self):
+        device_choice = input("Choose device (GPU/CPU): ").strip().lower()
+        if device_choice == "gpu" and torch.cuda.is_available():
+            self.device = "cuda"
+        else:
+            self.device = "cpu"
+        print_colored(f"Selected {self.device} device.", color=Fore.MAGENTA)
 
-            split_text = inferred_text.split(".")
-            if mode == "short" and len(split_text) > 1:
-                inferred_text = split_text[0] + "."
-            elif mode == "medium" and len(split_text) > 2:
-                inferred_text = ".".join(split_text[:2]) + "."
-            elif mode == "long" and len(split_text) > 3:
-                inferred_text = ".".join(split_text[:3]) + "."
+    def choose_precision(self):
+        precision_choice = input("Choose precision (float32/float16): ").strip().lower()
+        if precision_choice == "float16":
+            self.precision = "float16"
+        else:
+            self.precision = "float32"
+        print_colored(f"Selected {self.precision} precision.", color=Fore.GREEN)
 
-            return inferred_text.strip()
-        except Exception as e:
-            print(f"An error occurred during text inferencing: {str(e)}")
-            traceback.print_exc()
-            return ""
+    def toggle_tts(self):
+        self.tts_enabled = not self.tts_enabled
+        status = 'enabled' if self.tts_enabled else 'disabled'
+        print_colored(f"TTS {status}.", color=Fore.BLUE)
 
-    def chat_with_assistant(self, max_length=100, temperature=0.7, top_p=0.9, max_context_tokens=2048):
-        constant_context = "System: This is a conversation between a user and an AI assistant. The user can ask the assistant questions, seek advice, or engage in casual conversation."
+    def chat_with_assistant(self):
+        system_prompt = "You are a helpful, respectful, and honest assistant."
+        context_history = f"<s>[INST] <<SYS>> {system_prompt} <</SYS>>\nAssistant: How may I help you?\n"
 
-        context_history = constant_context + " Assistant: "
-        stop_characters = ".!?"
+        print_colored("Assistant: How may I help you?", color=Fore.CYAN)
 
         while True:
-            user_input = input("You: ")
+            # Print "You: " with color only
+            print_colored("You: ", color=Fore.LIGHTYELLOW_EX, end='')
+
+            user_input = input()
             if user_input.lower() in ["quit", "exit"]:
                 break
 
-            context_history += f"User: {user_input} Assistant: "
+            context_history += f"[user_message: {user_input}]\n"
 
-            tokens = self.tokenizer.encode(context_history, add_special_tokens=False)
-            if len(tokens) > max_context_tokens:
-                tokens = tokens[-max_context_tokens:]
-            context = self.tokenizer.decode(tokens)
+            inputs = self.tokenizer.encode_plus(
+                context_history, return_tensors='pt', padding=True, truncation=True, max_length=4096
+            )
+            inputs = inputs.to(self.device)
 
             try:
-                inferred_text = self.infer_text(f"{context}", max_length=max_length, temperature=temperature, top_p=top_p)
+                generation_length = 1000
+                generation_config = {
+                    'input_ids': inputs['input_ids'],
+                    'max_length': len(inputs['input_ids'][0]) + generation_length,
+                    'temperature': 0.7,
+                    'top_p': 0.9,
+                    'top_k': 50,
+                    'pad_token_id': self.tokenizer.eos_token_id,
+                    'do_sample': True,
+                    'early_stopping': True
+                }
+
+                outputs = self.model.generate(**generation_config)
+                generated_text = self.tokenizer.decode(outputs[0], skip_special_tokens=True)
+
+                # Extracting assistant's response
+                assistant_response = generated_text.split(f"[user_message: {user_input}]")[-1]
+                assistant_response = assistant_response.split("<s>")[0].strip()
+
+                # Check if the response already starts with "Assistant:"
+                if not assistant_response.startswith("Assistant:"):
+                    assistant_response = f"Assistant: {assistant_response}"
+
+                print_colored(assistant_response, color=Fore.CYAN)
+
+                context_history += f"<s>[INST] {assistant_response} </s><s>[INST]"
+
+                if self.verbose:
+                    logging.debug(f"Current context: {context_history}")
+
             except Exception as e:
-                print(f"An error occurred: {e}")
-                traceback.print_exc()
-                continue
+                print_colored("An error occurred during generation.", color=Fore.RED)
+                logging.error("An error occurred during generation:", exc_info=True)
 
-            assistant_responses = inferred_text.split("Assistant: ")
-            if len(assistant_responses) > 1:
-                assistant_response = assistant_responses[-1].split("User:")[0].strip()
-                if not assistant_response:
-                    assistant_response = "I'm not sure how to respond to that."
-            else:
-                assistant_response = "Sorry, I didn't get that."
-
-            print(f"Assistant: {assistant_response}")
-            context_history += f"{assistant_response} "
+    def extract_response(self, generated_text, user_input):
+        split_text = generated_text.split(f"[user_message: {user_input}]")
+        if len(split_text) > 1:
+            return split_text[1].split("<s>")[0].strip()
+        else:
+            return "I'm sorry, I couldn't generate a proper response."
 
 def select_path():
     root = tk.Tk()
@@ -128,74 +165,41 @@ def select_path():
     return folder_selected
 
 def display_menu():
-    print(Fore.CYAN + "\nInfernoLM: The Language Model Inferencer\n")
-    print("1. Infer Text")
-    print("2. Switch Model")
-    print("3. Toggle Real-time Token Output")
-    print("4. Chat with Assistant")
-    print("5. Exit")
-    print(Style.RESET_ALL)
+    menu_text = "\nInfernoLM: The Language Model Inferencer\n\n" + \
+                "1. Load Model\n" + \
+                "2. Chat with Assistant\n" + \
+                "3. Toggle Verbose Logging\n" + \
+                "4. Exit\n" + \
+                "5. Toggle Text-to-Speech\n"
+    print_colored(menu_text, color=Fore.YELLOW, attrs=['bold'])
 
 def main():
-    print(Fore.GREEN + "Welcome to InfernoLM: The Language Model Inferencer!")
-    model_path = select_path()
-    if not model_path:
-        print("No directory selected. Exiting.")
-        return
+    print_colored("Welcome to InfernoLM: The Language Model Inferencer!", color=Fore.GREEN, attrs=['bold'])
 
-    device_choice = input("\nSelect the device (cpu/gpu): ")
-    if device_choice == "gpu":
-        if torch.cuda.is_available():
-            device_choice = "cuda"
-            precision = input("Select the precision (float32 or float16): ")
-        else:
-            print("GPU not found. Defaulting to CPU.")
-            device_choice = "cpu"
-            precision = "float32"
-    else:
-        precision = "float32"
+    tts_strategy = Pyttsx3Strategy()
 
-    inferencer = InfernoLM(device=device_choice, precision=precision, model_path=model_path)
-    real_time_output = False
+    inferencer = InfernoLM(tts_strategy=tts_strategy)
+
     while True:
         display_menu()
-        choice = input("Enter your choice (1, 2, 3, 4, or 5): ")
+        choice = input("Enter your choice (1-5): ")
         if choice == "1":
-            prompt = input("\nEnter your prompt: ")
-            mode = input("Select the mode (short, medium, long, full, custom): ")
-
-            if mode == "custom":
-                max_length = int(input("Define the maximum length (e.g., 100): "))
-            else:
-                max_length = 500
-
-            temperature = float(input("Set the temperature (e.g., 0.7): "))
-            print(Fore.YELLOW)
-            inferred_text = inferencer.infer_text(prompt, mode, max_length, temperature, real_time=real_time_output)
-            print("\nInferred Text:")
-            print(inferred_text)
+            inferencer.load_model()
         elif choice == "2":
-            model_path = select_path()
-            if not model_path:
-                print("No directory selected. Retaining the current model.")
-                continue
-            del inferencer.model
-            del inferencer.tokenizer
-            torch.cuda.empty_cache()
-            gc.collect()
-            inferencer = InfernoLM(device=device_choice, precision=precision, model_path=model_path)
+            if inferencer.model_path:
+                print_colored("\nEntering Chatbot Mode. Type 'quit' or 'exit' to leave.", color=Fore.BLUE)
+                inferencer.chat_with_assistant()
+            else:
+                print_colored("No model loaded. Please load a model first.", color=Fore.RED)
         elif choice == "3":
-            real_time_output = not real_time_output
-            status = "ON" if real_time_output else "OFF"
-            print(f"Real-time Token Output is now {status}")
+            inferencer.toggle_verbose_logging()
         elif choice == "4":
-            print("\nEntering Chatbot Mode. Type 'quit' or 'exit' to leave.")
-            inferencer.chat_with_assistant(max_length=500, temperature=0.7, top_p=0.9)
-        elif choice == "5":
-            print(Fore.GREEN + "\nThank you for using InfernoLM. Farewell!")
+            print_colored("\nThank you for using InfernoLM. Farewell!", color=Fore.GREEN)
             break
+        elif choice == "5":
+            inferencer.toggle_tts()
         else:
-            print(Fore.RED + "\nInvalid selection. Please enter 1, 2, 3, 4, or 5.")
+            print_colored("\nInvalid selection. Please enter a number between 1 and 5.", color=Fore.RED)
 
 if __name__ == "__main__":
     main()
